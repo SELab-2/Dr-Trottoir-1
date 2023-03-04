0307eb4ff07d2b8527ce9e4375c5d78452e1ad0e
Jens Pots <jens@jenspots.com>
2023-03-05 00:42:34 +0100

    feature: basic user route

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,6 +9,13 @@ const PORT_NUMBER = 8080;
 
 const app = express();
 
+// JSON API support
+app.use(express.json({
+    inflate: true,
+    strict: true,
+    type: 'application/json',
+}));
+
 // Helmet adds many headers for more secure connections
 app.use(helmet());
 
@@ -19,12 +26,33 @@ app.use(morgan('dev'));
 app.use(compression());
 
 // Authentication. For now, we just remember the user at the other end
-app.use(async (res, req, next) => {
-    res.user = await prisma.user.findFirst({
-        where: {
-            id: 0,
+app.use(async (req, res, next) => {
+    let id: number | null = null;
+    const value = req.header('Authentication');
+
+    // Parse the header and try to retrieve the identifier of the user.
+    // TODO: actually use some form of authentication :)
+    try {
+        id = parseInt(value ?? '');
+    } catch (e) {
+        // Do nothing yet
+    }
+
+    // If there is an identifier, attempt to retrieve the corresponding user
+    req.user = null;
+    if (id !== null) {
+        try {
+            req.user = await prisma.user.findUnique({
+                where: {
+                    id: id
+                }
+            });
+        } catch (e) {
+            // Do nothing yet
         }
-    })
+    }
+
+    // We're done!
     next();
 })
 
--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,41 +1,156 @@
 import {prisma} from "../prisma";
 import express from "express";
+import {Prisma} from "@prisma/client";
 
 const DEFAULT_PAGE_SIZE = 1024;
 
 export class UserRouting {
     async getAll(req: express.Request, res: express.Response) {
+        if (!req.user?.super_student) {
+            res.status(403);
+            res.send("Unauthorized")
+            return;
+        }
+
         const limit = Number(req.query['limit'] ?? DEFAULT_PAGE_SIZE);
         const offset = Number(req.query['offset'] ?? 0);
+        const student = req.query['student'] == 'true' ? true : req.query['student'] == 'false' ? false : undefined;
+        const superStudent = req.query['super_student'] == 'true' ? true : req.query['super_student'] == 'false' ? false : undefined;
+        const admin = req.query['admin'] == 'true' ? true : req.query['admin'] == 'false' ? false : undefined;
+
+        if (Number.isNaN(limit) || Number.isNaN(offset)) {
+            // THROW ERROR
+        }
 
         const result = await prisma.user.findMany({
             take: limit,
             skip: offset,
+            where: {
+                student: student,
+                super_student: superStudent,
+                admin: admin,
+            },
+            include: {
+                address: true,
+                regions: true,
+                schedule: true,
+            }
         });
 
         return res.json(result);
     }
 
     async getOne(req: express.Request, res: express.Response) {
-        try {
-            const id = Number(req.params['id']);
+        const id = parseInt(req.params.id);
 
-            const result = await prisma.user.findFirst({
+        if (isNaN(id)) {
+            // THROW EXCEPTION
+        }
+
+        if (id !== req.user?.id || !req.user.super_student || !req.user.admin) {
+            res.status(403);
+            res.send("Unauthorized")
+            return;
+        }
+
+        const result = await prisma.user.findFirst({
+            where: {
+                id: id
+            },
+            include: {
+                address: true,
+                regions: true,
+                schedule: true,
+            }
+        });
+
+        return res.json(result);
+    }
+
+    async createOne(req: express.Request, res: express.Response) {
+        if (!req.user?.super_student || !req.user?.admin) {
+            res.status(403);
+            res.send("Unauthorized")
+            return;
+        }
+
+        const user = await prisma.user.create({
+            data: req.body,
+        });
+
+        return res.status(200).json(user);
+    }
+
+    async updateOne(req: express.Request, res: express.Response) {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) {
+            // THROW EXCEPTION
+        }
+
+        if (id !== req.user?.id && !req.user?.super_student && !req.user?.admin) {
+            res.status(403);
+            res.send("Unauthorized")
+            return;
+        }
+
+        const result = await prisma.user.update({
+            data: req.body,
+            where: {
+                id: id
+            },
+        });
+
+        return res.status(200).json(result);
+    }
+
+    async deleteOne(req: express.Request, res: express.Response) {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) {
+            return res.status(400).send("Invalid Request");
+        }
+
+        if (id !== req.user?.id && !req.user?.super_student && !req.user?.admin) {
+            res.status(403);
+            res.send("Unauthorized")
+            return;
+        }
+
+        // TODO: delete cascade in the database!
+        await prisma.userRegion.deleteMany({
+            where: {
+                user_id: id
+            },
+        })
+
+        try {
+            const result = await prisma.user.delete({
                 where: {
                     id: id
                 },
             });
 
-            return res.json(result);
+            return res.status(200).json(result);
         } catch (e) {
-            return res.json(e);
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                switch (e.code) {
+                    case 'P2025':
+                        return res.status(404).json({message: "Not Found"})
+                }
+            }
+
+            return res.status(500).json({message: "Internal Server Error"});
         }
     }
 
     toRouter(): express.Router {
         const router = express.Router();
-        router.get('/', (req: express.Request, res: express.Response) => this.getAll(req, res));
-        router.get('/:id', (req: express.Request, res: express.Response) => this.getOne(req, res));
+        router.get('/', this.getAll);
+        router.get('/:id', this.getOne);
+        router.post('/', this.createOne);
+        router.patch('/:id', this.updateOne);
+        router.delete('/:id', this.deleteOne);
         return router;
     }
 }
