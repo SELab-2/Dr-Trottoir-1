4154faaecceafc27bfa8cfa9418012a7f67c79c7
brent <brent-matthys@outlook.com>
2023-03-05 16:28:25 +0100

    code documentation

--- web/src/views/LoginScreen.vue
+++ web/src/views/LoginScreen.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="background">
         <div class="form">
+            <!-- Display the Dr. troittoir logo above the login form -->
             <v-img
                 contain
                 src="@/assets/images/drtroittoir_logo.png"
             ></v-img>
             <br>
+
+            <!-- The input field for the e-mail -->
             <v-text-field
                 v-model="email"
                 :prepend-inner-icon="'mdi-email'"
@@ -13,6 +16,8 @@
                 type="text"
                 required    
             ></v-text-field>
+
+            <!-- The input field for the password -->
             <v-text-field
                 v-model="password"
                 :prepend-inner-icon="'mdi-lock'"
@@ -22,7 +27,9 @@
                 @click:append-inner="showPsswd = !showPsswd"
                 bg
             ></v-text-field>
+            <!-- Div with help text and login button -->
             <div>
+                <!-- Help text for people who don't have an account yet -->
                 <p class="float-l">
                 Nog geen account? 
                     <span 
@@ -32,6 +39,7 @@
                         Contacteer ons
                     </span>
                 </p>
+                <!-- Login button -->
                 <v-btn 
                     class="float-r"
                     @click="handleSubmit"
@@ -42,6 +50,8 @@
              
         </div>
     </div>
+
+    <!-- Popup message containing detailed info about account creation. Will pop up when clicked on the text in the bottom div -->
     <v-snackbar
         v-model="snackbar"
     >
@@ -62,13 +72,18 @@
 export default{
     data() {
         return{
+            // property containing the inputed email value
             email: '',
+            // property containing the inputed password value
             password: '',
+            // propery keeping track if the password should be displayed in plain text or not
             showPsswd: false,
+            // property keeping track if the snackbar should be shown or not
             snackbar: false
         }
     },
     methods: {
+        // Function that handels the login event
         handleSubmit(){
             console.log('submited');
         }
@@ -78,7 +93,7 @@ export default{
 </script>
 <style lang="scss">
     
-
+    // backgroud div. Nedded to center form div
     .background{
         height: 100%;
         width: 100%;
@@ -87,6 +102,7 @@ export default{
         align-items: center;
     }
 
+    // div with input fields. Needed for responsive design
     .form {
         width: 600px;
         max-width: 90%;
@@ -94,16 +110,19 @@ export default{
         align-content: center;
     }
 
+    // makes the info text look like it's clickable
     .clickable-text{
         text-decoration: underline;
         cursor: pointer;
         color: #069;
     }
 
+    // let component live left in it's parent
     .float-l{
         float: left;
     }
 
+    // let component live right in it's parent
     .float-r{
         float: right;
     }
