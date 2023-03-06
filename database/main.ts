5cf3499c4707886ac47b00269e2dfc560d0bb15b
Janne Cools <janne.cools@ugent.be>
2023-03-06 18:59:03 +0100

    feat: toevoeging mock data voor progress en progress_image

--- database/main.ts
+++ database/main.ts
@@ -1,18 +1,20 @@
 import { createUser, createRegion, createUserRegion } from "./mock/user";
 import { createBuilding, createGarbage, createBuildingImages } from "./mock/building";
 import { createRound, createRoundBuilding } from "./mock/round";
-import { createSchedule } from "./mock/schedule";
+import { createSchedule, createProgress, createProgressImage } from "./mock/schedule";
 
 async function main() {
     //await createUser();
     //await createRegion();
-    //await createBuilding();
+    await createBuilding();
     //await createUserRegion();
     //await createGarbage();
     //await createRound();
     await createRoundBuilding();
     await createSchedule();
     await createBuildingImages();
+    await createProgress();
+    await createProgressImage();
 }
 
 // Actually call main
--- database/mock/schedule.ts
+++ database/mock/schedule.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient, ProgressImageType} from '@prisma/client'
 import { Chance } from "chance";
 
 const prisma = new PrismaClient()
@@ -7,8 +7,8 @@ const chance = new Chance();
 export async function createSchedule() {
     const rounds = await prisma.round.findMany();
     const users = await prisma.user.findMany();
-    const maxRound = rounds.length-1;
-    const maxUser = users.length-1;
+    const maxRound = rounds.length - 1;
+    const maxUser = users.length - 1;
     const round = rounds[chance.integer({min: 0, max: maxRound})];
     const user = users[chance.integer({min: 0, max:maxUser})];
     await prisma.schedule.create({
@@ -19,6 +19,50 @@ export async function createSchedule() {
             },
             user: {
                 connect: {id: user.id}
+            }
+        }
+    })
+}
+
+export async function createProgress() {
+    const schedules = await prisma.schedule.findMany();
+    const buildings = await prisma.building.findMany();
+    const maxSchedule = schedules.length - 1;
+    const maxBuilding = buildings.length - 1;
+    const schedule = schedules[chance.integer({min: 0, max: maxSchedule})];
+    const building = buildings[chance.integer({min: 0, max: maxBuilding})];
+    await prisma.progress.create({
+        data: {
+            report: chance.sentence(),
+            arrival: chance.date(),
+            departure: chance.date(),
+            schedule: {
+                connect: {id: schedule.id}
+            },
+            building: {
+                connect: {id: building.id}
+            }
+        }
+    })
+}
+
+export async function createProgressImage() {
+    const progresses = await prisma.progress.findMany();
+    const types : ProgressImageType[] = ['ARRIVAL', 'DEPARTURE', 'GARBAGE'];
+    const maxProgress = progresses.length - 1;
+    const progress = progresses[chance.integer({min: 0, max: maxProgress})];
+    const type = types[chance.integer({min: 0, max: 2})];
+    await prisma.progressImage.create({
+        data: {
+            type: type,
+            description: chance.sentence(),
+            progress: {
+                connect: {id: progress.id}
+            },
+            image: {
+                create: {
+                    time: chance.date()
+                }
             }
         }
     })
