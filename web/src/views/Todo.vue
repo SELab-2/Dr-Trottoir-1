f78c5c5e3bdcb4debc6ba4c129bddb86c36787b6
arne <vanheule.arne@ugent.be>
2023-03-07 18:09:17 +0100

    feat/temp: Added temporary page for pages that still need to be linked in the router to work on the navbar

---
+++ web/src/views/Todo.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Nog niet geimplementeerde pagina</h1>
+  <h2>Wat moet je doen?</h2>
+  <ul>
+    <li>Voeg de route toe aan de router in index.ts</li>
+    <li>Voeg deze ook toe aan de router lijst in App.Vue</li>
+  </ul>
+</template>
