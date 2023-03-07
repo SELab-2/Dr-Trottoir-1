7fc798500f6bef0f45d93061b4b3df8987ae3157
arne <vanheule.arne@ugent.be>
2023-03-07 17:29:30 +0100

    feat: Added a dashboardpage that can be shown after someone succeeds to login. Note that this is temporary and it's only purpose at this moment is to be a role selector

---
+++ web/src/views/DashBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Pick a role to view the webpage as -->
+  <div class="d-flex align-center flex-column bg-grey-lighten-4 pa-6">
+    <h2>Select your current role: </h2>
+    <v-btn-toggle divided>
+      <v-btn @click="setStudent">Student</v-btn>
+      <v-btn @click="setSuperStudent">Superstudent</v-btn>
+      <v-btn @click="setSyndicus">Syndicus</v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import { auth } from '@/scripts/auth'
+
+function setStudent() {
+  auth.isLoggedin = true;
+  auth.roles.superstudent = false;
+  auth.roles.syndicus = false;
+}
+
+function setSuperStudent() {
+  auth.isLoggedin = true;
+  auth.roles.superstudent = true;
+  auth.roles.syndicus = false;
+}
+
+function setSyndicus() {
+  auth.isLoggedin = true;
+  auth.roles.superstudent = false;
+  auth.roles.syndicus = true;
+}
+</script>
