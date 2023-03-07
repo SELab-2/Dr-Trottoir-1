25a5713f4ef173446c7f16fc21dbe8ed15ce7ed0
arne <vanheule.arne@ugent.be>
2023-03-07 17:09:17 +0100

    feat: Added basic layout to the website so it should always have space for a navigation drawer on top

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <v-app>
+    <v-app-bar v-if="auth.isLoggedin" prominent>
+      <v-btn>Test 1</v-btn>
+
+      <v-btn>Test 2</v-btn>
+
+    </v-app-bar>
+
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script lang="ts" setup>
+
+//TODO: remove this import and replace it for propper authentication in finished product
+import { auth } from '@/scripts/auth'
+
+</script>
