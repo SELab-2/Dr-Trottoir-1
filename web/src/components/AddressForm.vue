2b2562754ab7250a2b5e11c361f4b8bf115a53ec
brent <brent-matthys@outlook.com>
2023-03-09 09:06:56 +0100

    start styling page

--- web/src/components/AddressForm.vue
+++ web/src/components/AddressForm.vue
@@ -1,40 +1,68 @@
 <template>
     <div>
-        <!-- Text input field for the street name -->
-        <v-text-field
-            v-model="address.street"
-            label="Straat"
-            type="text"
-            required
-            @update:model-value="$emit('onUpdate', address)"
-        ></v-text-field>
+        <v-row class="py-0 my-0">
+            <v-col
+                cols="1"
+                style="min-width: 100px; max-width: 100%;"
+                class="flex-grow-1 flex-shrink-0 py-0 my-0"
+            >
+                <!-- Text input field for the street name -->
+                <v-text-field
+                    
+                    v-model="address.street"
+                    label="Straat"
+                    type="text"
+                    required
+                    @update:model-value="$emit('onUpdate', address)"
+                ></v-text-field>
+            </v-col>
+            <v-col
+                cols="3"
+                class="flex-grow-0 flex-shrink-0 py-0 my-0"
+            >
+                <!-- Text input field for the house number -->
+                <v-text-field
 
-        <!-- Text input field for the house number -->
-        <v-text-field
-            v-model="address.number"
-            label="Huisnummer"
-            type="number"
-            required
-            @update:model-value="$emit('onUpdate', address)"
-        ></v-text-field>
+                    v-model="address.number"
+                    label="Huisnummer"
+                    type="number"
+                    required
+                    @update:model-value="$emit('onUpdate', address)"
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row class="py-0 my-0">
+            <v-col
+                cols="1"
+                style="min-width: 100px; max-width: 100%;"
+                class="flex-grow-1 flex-shrink-0 py-0 my-0"
+            >
+                <!-- Text input field for the city name -->
+                <v-text-field
+                    v-model="address.city"
+                    label="Stad"
+                    type="text"
+                    required
+                    @update:model-value="$emit('onUpdate', address)"
+                ></v-text-field>
+            </v-col>
+            <v-col
+                cols="3"
+                class="flex-grow-0 flex-shrink-0 py-0 my-0"
+            >
+                <!-- Text input field for the zip code -->
+                <v-text-field
+                    v-model="address.zip_code"
+                    label="Post code"
+                    type="number"
+                    required
+                    @update:model-value="$emit('onUpdate', address)"
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        
 
-        <!-- Text input field for the city name -->
-        <v-text-field
-            v-model="address.city"
-            label="Stad"
-            type="text"
-            required
-            @update:model-value="$emit('onUpdate', address)"
-        ></v-text-field>
-
-        <!-- Text input field for the zip code -->
-        <v-text-field
-            v-model="address.zip_code"
-            label="Post code"
-            type="number"
-            required
-            @update:model-value="$emit('onUpdate', address)"
-        ></v-text-field>
+        
     </div>
 
 </template>
--- web/src/views/UserCreation.vue
+++ web/src/views/UserCreation.vue
@@ -1,86 +1,101 @@
 <template>
 
-    <div>
-        
-        <!-- Text input field for the first name -->
-        <v-text-field
-            v-model="first_name"
-            label="Voornaam"
-            type="text"
-            required    
-        ></v-text-field>
+    <div class="background">
+        <div class="form">
+            <v-row class="py-0 my-0">
+                <v-col
+                cols="1"
+                style="min-width: 100px; max-width: 100%;"
+                class="flex-grow-1 flex-shrink-0 py-0 my-0"
+                >
+                    <!-- Text input field for the first name -->
+                    <v-text-field
+                        v-model="first_name"
+                        label="Voornaam"
+                        type="text"
+                        required    
+                    ></v-text-field>
+                </v-col>
+                
+                <v-col
+                cols="1"
+                style="min-width: 100px; max-width: 100%;"
+                class="flex-grow-1 flex-shrink-0 py-0 my-0"
+                >
+                    <!-- Text input field for the last name -->
+                    <v-text-field
+                        v-model="last_name"
+                        label="Achternaam"
+                        type="text"
+                        required    
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            
+            <!-- Text input field for the phone number -->
+            <v-text-field
+                v-model="phone_number"
+                :prepend-inner-icon="'mdi-phone'"
+                label="Telefoon nummer"
+                type="text"
+                required    
+            ></v-text-field>
 
-        <!-- Text input field for the last name -->
-        <v-text-field
-            v-model="last_name"
-            label="Achternaam"
-            type="text"
-            required    
-        ></v-text-field>
 
-        <!-- Text input field for the phone number -->
-        <v-text-field
-            v-model="phone_number"
-            :prepend-inner-icon="'mdi-phone'"
-            label="Telefoon nummer"
-            type="text"
-            required    
-        ></v-text-field>
+            <!-- Text input field for the email -->
+            <v-text-field
+                v-model="email"
+                :prepend-inner-icon="'mdi-email'"
+                label="E-mail"
+                type="text"
+                required    
+            ></v-text-field>
 
+            <!-- Address input form of the user -->
+            <AddressForm
+                @onUpdate="(newAddress) => address = newAddress"
+            ></AddressForm>
 
-        <!-- Text input field for the email -->
-        <v-text-field
-            v-model="email"
-            :prepend-inner-icon="'mdi-email'"
-            label="E-mail"
-            type="text"
-            required    
-        ></v-text-field>
+            <!-- Text input field for the password-->
+            <v-text-field
+                v-model="password1"
+                :prepend-inner-icon="'mdi-lock'"
+                :append-inner-icon="showPsswd ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="showPsswd ? 'text' : 'password'"
+                label="Wachtwoord"
+                @click:append-inner="showPsswd = !showPsswd"
+                bg
+            ></v-text-field>
 
-        <!-- Address input form of the user -->
-        <AddressForm
-            @onUpdate="(newAddress) => address = newAddress"
-        ></AddressForm>
+            <!-- Text input field for the password confirmation-->
+            <v-text-field
+                v-model="password2"
+                :prepend-inner-icon="'mdi-lock'"
+                :append-inner-icon="showPsswd ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="showPsswd ? 'text' : 'password'"
+                label="Bevestig wachtwoord"
+                @click:append-inner="showPsswd = !showPsswd"
+                bg
+            ></v-text-field>
 
-        <!-- Text input field for the password-->
-        <v-text-field
-            v-model="password1"
-            :prepend-inner-icon="'mdi-lock'"
-            :append-inner-icon="showPsswd ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPsswd ? 'text' : 'password'"
-            label="Wachtwoord"
-            @click:append-inner="showPsswd = !showPsswd"
-            bg
-        ></v-text-field>
-
-        <!-- Text input field for the password confirmation-->
-        <v-text-field
-            v-model="password2"
-            :prepend-inner-icon="'mdi-lock'"
-            :append-inner-icon="showPsswd ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPsswd ? 'text' : 'password'"
-            label="Bevestig wachtwoord"
-            @click:append-inner="showPsswd = !showPsswd"
-            bg
-        ></v-text-field>
-
-        <!-- Selection box to determine the roles-->
-        <v-select
-            chips
-            label="Rol"
-            :items="['Student', 'Superstudent', 'Syndicus', 'Admin']"
-            multiple
-            v-model="roles"
-        ></v-select>
-        <div v-if="roles.includes('Syndicus')">
-            <BuildingForm
-                @onUpdate="(newBuilding) => building = newBuilding"
-            >
-            </BuildingForm>
+            <!-- Selection box to determine the roles-->
+            <v-select
+                chips
+                label="Rol"
+                :items="['Student', 'Superstudent', 'Syndicus', 'Admin']"
+                multiple
+                v-model="roles"
+            ></v-select>
+            <div v-if="roles.includes('Syndicus')">
+                <BuildingForm
+                    @onUpdate="(newBuilding) => building = newBuilding"
+                >
+                </BuildingForm>
+            </div>
+            <v-btn>
+                Maak Account
+            </v-btn>
         </div>
-        <v-btn>
-            Maak Account
-        </v-btn>
     </div>
 </template>
 
@@ -137,6 +152,22 @@ const roles: Ref<String[]> = ref([])
 
 </script>
 
-<style lang="sass">
+<style lang="scss">
+
+    .background{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    .form {
+        width: 720px;
+        max-width: 90%;
+        padding: 10px;
+        align-content: center;
+    }
 
 </style>
