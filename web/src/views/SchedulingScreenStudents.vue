4190081e4f68cce84d03cafae900d4f9d2c1830f
Arnout_Allaert <arnout.allaert@ugent.be>
2023-03-09 15:29:15 +0100

    dagen in v-card gestoken, dit ziet er iets mooier uit

--- web/src/views/SchedulingScreenStudents.vue
+++ web/src/views/SchedulingScreenStudents.vue
@@ -2,11 +2,11 @@
   <h1>Planningsoverzicht</h1>
   <v-list>
     <v-list-item v-for="day in days" :key="day.name">
-      {{ day.name }}
+      <v-card v-text="day.name"></v-card>
       <v-table>
         <thead>
         <tr>
-          <th class="text-left">Name</th>
+          <th class="text-left">Building</th>
           <th class="text-left">Address</th>
           <th class="text-left">Deadline</th>
           <th class="text-left">Info</th>
@@ -34,34 +34,34 @@ export default {
       days: {
         monday: {name: "monday", buildings: [
           {name: "gebouw1", address: "coolestraat 69", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 0)},
-          {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
-            , deadline: new Date(2023, 0O2, 0O6, 20, 30)},
-          {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 21, 0)},
-          {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 30)}]},
+              , deadline: new Date(2023, 0O2, 0O6, 19, 0)},
+            {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
+              , deadline: new Date(2023, 0O2, 0O6, 20, 30)},
+            {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 21, 0)},
+            {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 19, 30)}]},
         tuesday: {name: "tuesday", buildings: [
           {name: "gebouw1", address: "coolestraat 69", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 30)},
-          {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 0)},
-          {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 22, 0)},
-          {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 0)}]},
+              , deadline: new Date(2023, 0O2, 0O6, 19, 30)},
+            {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
+              , deadline: new Date(2023, 0O2, 0O6, 19, 0)},
+            {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 22, 0)},
+            {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 19, 0)}]},
         wednesday: {name: "wednesday", buildings: [
           {name: "gebouw1", address: "coolestraat 69", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 21, 30)},
-          {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
-            , deadline: new Date(2023, 0O2, 0O6, 19, 45)},
-          {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 20, 0)},
-          {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
-            , deadline: new Date(2023, 0O2, 0O6, 20, 30)}]},
-      }
+              , deadline: new Date(2023, 0O2, 0O6, 21, 30)},
+            {name: "gebouw2", address: "anderestraat 420", info: "wegens werken dient de achteringang genomen te worden"
+              , deadline: new Date(2023, 0O2, 0O6, 19, 45)},
+            {name: "gebouw3", address: "teamstraat 1", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 20, 0)},
+            {name: "gebouw4", address: "geeninspiratiestraat 13", info: "meer info/opmerkingen"
+              , deadline: new Date(2023, 0O2, 0O6, 20, 30)}]},
+      },
     }
-  }
+  },
 }
 </script>
 
