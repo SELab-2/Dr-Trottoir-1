ac4857a9b4fc6b187533231ec843689d798de6dd
Janne Cools <janne.cools@ugent.be>
2023-03-10 13:41:27 +0100

    feat: toevoeging route voor Round

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,6 +9,7 @@ import { ErrorHandler } from "./errors/error_handler";
 import { ScheduleRouting } from "./routes/schedule";
 import { AuthRouting } from "./routes/auth";
 import { ActionRouting } from "./routes/action";
+import { RoundRouting } from "./routes/round";
 import passport from "passport";
 import crypto from "crypto";
 import session from "express-session";
@@ -61,6 +62,7 @@ app.use("/user", new UserRouting().toRouter());
 app.use("/building", new BuildingRouting().toRouter());
 app.use("/schedule", new ScheduleRouting().toRouter());
 app.use("/action", new ActionRouting().toRouter());
+app.use("/round", new RoundRouting().toRouter());
 
 // Finally, an error handler
 app.use(ErrorHandler.handle);
