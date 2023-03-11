abc2aca7ce4f60897414e597a5922c9168922ecc
Ludovic <ludovic.verstraete@ugent.be>
2023-03-11 19:25:49 +0100

    feat: layout zonder add images

---
+++ web/src/components/addImage.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <v-container>
+      <v-form>
+        <v-row>
+          <v-col cols="3">
+            <v-img
+              v-if="preview"
+              :src="preview"
+              lazySrc="../assets/images/defaultImage.png"
+              v-model="preview"
+              width="350"
+              height="350"
+            ></v-img>
+          </v-col>
+          <v-col>
+            <v-file-input
+              single
+              v-model="image"
+              label="Select Image"
+              accept="image/*"
+              prepend-icon="mdi-image"
+              @change="previewImage"
+            ></v-file-input>
+            <v-textarea label="Comments" v-model="comments"></v-textarea>
+            <v-text-field label="Image Label" v-model="label"></v-text-field>
+          </v-col>
+        </v-row>
+        <div class="d-flex justify-center align-center">
+          <v-btn @click="submit" color="primary">Submit</v-btn>
+        </div>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import pic from "../assets/images/defaultImage.png";
+export default {
+  setup() {
+    const preview = ref(pic);
+    const image = ref(null);
+    const label = ref("");
+    const comments = ref("");
+    const previewImage = () => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        preview.value = reader.result;
+      };
+      reader.readAsDataURL(image.value[0]);
+    };
+    const submit = () => {
+      try {
+        const formData = new FormData();
+        formData.append("image", image.value);
+        formData.append("label", label.value);
+        formData.append("comment", comments.value);
+        console.log("verzonden");
+        for (const value of formData.values()) {
+          console.log(value);
+        }
+        //const response = await axios.post("/images", formData);
+        //console.log(response.data);
+        // reset form after submit
+        //file.value = null;
+        preview.value = pic;
+        label.value = "";
+        comments.value = "";
+        image.value = null;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    return {
+      preview,
+      image,
+      label,
+      comments,
+      previewImage,
+      submit,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.d-flex {
+  display: flex;
+}
+.justify-center {
+  justify-content: center;
+}
+.align-center {
+  align-items: center;
+}
+</style>
--- web/src/views/BuildingMaker.vue
+++ web/src/views/BuildingMaker.vue
@@ -1,11 +1,66 @@
 <template>
   <v-container>
-    <v-form @submit.prevent="submitForm">
-      <v-text-field v-model="name" label="Building Name"></v-text-field>
-      <v-text-field v-model="ivagoid" label="Ivago id"></v-text-field>
-      <v-text-field v-model="syndicus" label="Syndicus"></v-text-field>
-      <v-text-field v-model="address" label="Address"></v-text-field>
-      <v-file-input v-model="file" label="Manual"></v-file-input>
+    <v-form>
+      <v-row>
+        <v-col cols="3">
+          <v-img :src="previewBuildingImage"></v-img>
+        </v-col>
+        <v-col>
+          <v-file-input
+            label="Building Image"
+            prepend-icon="mdi-image"
+          ></v-file-input>
+          <v-text-field label="Building Name"></v-text-field>
+          <v-text-field label="Ivago id"></v-text-field>
+          <v-select
+            label="Syndicus"
+            :items="[
+              'California',
+              'Colorado',
+              'Florida',
+              'Georgia',
+              'Texas',
+              'Wyoming',
+            ]"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-divider :thickness="5" class="pa-md-4 mx-lg-auto"></v-divider>
+      <v-row>
+        <v-col cols="3">
+          <v-img :src="dummyMap" v-model="dummyMap"></v-img>
+        </v-col>
+        <v-col>
+          <v-row>
+            <v-col>
+              <v-text-field label="Longitude"></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field label="Latitude"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field label="Straat"></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field label="Huisnummer"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field label="Stad"></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-text-field label="Post code"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-divider :thickness="5" class="pa-md-4 mx-lg-auto"></v-divider>
+      <v-file-input label="Manual"></v-file-input>
+      <v-divider :thickness="5" class="pa-md-4 mx-lg-auto"></v-divider>
+      <AddImage> hallo </AddImage>
       <v-btn type="submit">Submit</v-btn>
       <!--image id, adress inline , meerdere afbeeldigen met commetns
       onder elkaar als er 1 gemaakt is kunnen er meerdere bij
@@ -15,38 +70,22 @@
 </template>
 
 <script>
-//import axios from "axios";
-
+import { ref } from "vue";
+import dummy from "../assets/images/dummyMap.png";
+import defaultImg from "../assets/images/defaultImage.png";
+import AddImage from "../components/addImage.vue";
 export default {
-  data() {
-    return {
-      name: "",
-      ivagoid: "",
-      syndicus: "",
-      address: "",
-      file: null,
-    };
-  },
-  methods: {
-    submitForm() {
-      const formData = new FormData();
-      formData.append("name", this.name);
-      formData.append("ivagoid", this.ivagoid);
-      formData.append("syndicus", this.syndicus);
-      formData.append("address", this.address);
-      formData.append("file", this.file);
-
-      //   axios
-      //     .post("/building", formData)
-      //     .then((response) => {
-      //       console.log(response.data);
-      //       // handle successful response
-      //     })
-      //     .catch((error) => {
-      //       console.log(error.response.data);
-      //       // handle error response
-      //     });
-    },
+  setup() {
+    const dummyMap = dummy;
+    // const name = ref("");
+    // const ivagoid = ref("");
+    // const syndicus = ref("");
+    // const adress = ref("");
+    // const file = ref(null);
+    const previewBuildingImage = defaultImg;
+    // const mapImage = ref(null);
+    // return name, ivagoid, syndicus, adress, file, buildingImage, mapImage;
+    return { dummyMap, previewBuildingImage };
   },
 };
 </script>
