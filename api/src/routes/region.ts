daca0c34fde4fecd2cae485fbdc28c55723da83f
Jozef Jankaj <jozef.jankaj@ugent.be>
2023-03-12 17:58:00 +0100

    Region: users can now be added at creation time

--- api/src/routes/region.ts
+++ api/src/routes/region.ts
@@ -1,51 +1,77 @@
-import { CustomRequest, Routing } from "./routing";
+import {CustomRequest, Routing} from "./routing";
 import express from "express";
-import { Parser } from "../parser";
-import { prisma } from "../prisma";
-import { Auth } from "../auth/auth";
+import {Parser} from "../parser";
+import {prisma} from "../prisma";
+import {Auth} from "../auth/auth";
 
 export class RegionRouting extends Routing {
-    @Auth.authorization({ superStudent: true })
+    @Auth.authorization({superStudent: true})
     async getAll(req: CustomRequest, res: express.Response) {
         const joins = Parser.stringArray(req.query.join, []);
+        console.log(req.query)
         const result = await prisma.region.findMany({
             take: Parser.number(req.query["take"], 1024),
             skip: Parser.number(req.query["skip"], 0),
-
+            where: {
+                id: Parser.number(req.query["id"]),
+                name: Parser.string(req.query["name"])
+            },
             include: {
-                users: joins?.includes("users"),
+                users: {
+                    include: {
+                        user: joins?.includes("user")
+                    }
+                }
             },
         });
 
         return res.json(result);
     }
 
-    @Auth.authorization({ student: true })
+    @Auth.authorization({student: true})
     async getOne(req: CustomRequest, res: express.Response) {
         const joins = Parser.stringArray(req.query.join, []);
         const result = await prisma.region.findUniqueOrThrow({
             where: {
                 id: Parser.number(req.params["id"]),
             },
-
             include: {
-                users: joins?.includes("users"),
+                users: {
+                    include: {
+                        user: joins?.includes("user")
+                    }
+                },
             },
         });
 
         return res.json(result);
     }
 
-    @Auth.authorization({ superStudent: true })
+    @Auth.authorization({superStudent: true})
     async createOne(req: CustomRequest, res: express.Response) {
+        const connectedUsers = req.body["users"].map((id: string) => {
+            return {
+                user: {
+                    connect: {
+                        id: id
+                    }
+                }
+            }
+        });
+
         const result = await prisma.region.create({
-            data: req.body,
+            data: {
+                name: req.body["name"],
+                users: {
+                    create: connectedUsers
+                }
+            }
         });
 
         return res.status(201).json(result);
     }
 
-    @Auth.authorization({ superStudent: true })
+    @Auth.authorization({superStudent: true})
     async updateOne(req: CustomRequest, res: express.Response) {
         const result = await prisma.region.update({
             data: req.body,
@@ -57,7 +83,7 @@ export class RegionRouting extends Routing {
         return res.status(200).json(result);
     }
 
-    @Auth.authorization({ superStudent: true })
+    @Auth.authorization({superStudent: true})
     async deleteOne(req: CustomRequest, res: express.Response) {
         const result = await prisma.region.delete({
             where: {
