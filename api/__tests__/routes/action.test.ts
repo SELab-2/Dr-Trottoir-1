0a053bc1f90599cf3b3697f87698cadb5e0dd5c1
Janne Cools <janne.cools@ugent.be>
2023-03-13 17:52:53 +0100

    test: toevoeging test voor ActionRouting voor het bewerken van een entry

--- api/__tests__/routes/action.test.ts
+++ api/__tests__/routes/action.test.ts
@@ -7,17 +7,17 @@ describe("Server.ts tests", () => {
     });
 });
 
-describe("Test ActionRouting", () => {
+describe("Test ActionRouting successful requests", () => {
     /*
     Bij elke test wordt een nieuwe action toegevoegd aan de databank. Op deze manier kunnen er geen fouten ontstaan
     door entries die uit de databank verwijderd zijn. De data waarmee gewerkt wordt, zal dus sowieso in de databank
     aanwezig zijn. Deze data wordt op het einde van de test weer verwijderd uit de databank.
      */
 
-    test("Test creating new action", async () => {
+    test("Test creating and deleting new action", async () => {
         const session = await request(app);
 
-        // Eerst moet er ingelogd worden om autorisatie te krijgen. TODO: fixen: na inloggen krijg ik geen autorisatie
+        // Eerst moet er ingelogd worden om autorisatie te krijgen.
         const resultLogin = await session
             .post("/auth/login")
             .send({ username: "jens.pots@ugent.be", password: "password" });
@@ -35,33 +35,6 @@ describe("Test ActionRouting", () => {
         expect(resultAdd.body["description"]).toEqual("new action for testing");
 
         // Uiteindelijk moet de nieuwe actie terug verwijderd worden.
-        const resultDelete = await session
-            .delete("/action/" + resultAdd.body["id"])
-            .set("Cookie", [cookies]);
-        expect(resultDelete.status).toEqual(200);
-    });
-
-    test("Test deleting an action", async () => {
-        const session = await request(app);
-
-        // Eerst moet er ingelogd worden om autorisatie te krijgen.
-        const resultLogin = await session
-            .post("/auth/login")
-            .send({ username: "jens.pots@ugent.be", password: "password" });
-        expect(resultLogin.status).toEqual(302);
-        expect(resultLogin.headers).toHaveProperty("set-cookie");
-
-        const cookies = resultLogin.headers["set-cookie"].pop().split(";")[0];
-
-        // Nieuwe action toevoegen
-        const resultAdd = await session
-            .post("/action")
-            .send({ description: "new action for testing" })
-            .set("Cookie", [cookies]);
-        expect(resultAdd.status).toEqual(201);
-        expect(resultAdd.body["description"]).toEqual("new action for testing");
-
-        // Action terug verwijderen
         const resultDelete = await session
             .delete("/action/" + resultAdd.body["id"])
             .set("Cookie", [cookies]);
@@ -101,7 +74,7 @@ describe("Test ActionRouting", () => {
         expect(resultDelete.status).toEqual(200);
     });
 
-    test("Test editing existing action", async () => {
+    test("Test updating existing action", async () => {
         const session = await request(app);
 
         // Eerst moet er ingelogd worden om autorisatie te krijgen.
@@ -113,7 +86,7 @@ describe("Test ActionRouting", () => {
 
         const cookies = resultLogin.headers["set-cookie"].pop().split(";")[0];
 
-        // Nieuwe action toevoegen
+        // Nieuwe waarde toevoegen
         const resultAdd = await session
             .post("/action")
             .send({ description: "new action for testing" })
@@ -121,15 +94,24 @@ describe("Test ActionRouting", () => {
         expect(resultAdd.status).toEqual(201);
         expect(resultAdd.body["description"]).toEqual("new action for testing");
 
-        //const
+        // Toegevoegde waarde bewerken
+        const resultUpdate = await session
+            .patch("/action/" + resultAdd.body["id"])
+            .send({ description: "updated action description" })
+            .set("Cookie", [cookies]);
+        expect(resultUpdate.status).toEqual(200);
+        expect(resultUpdate.body["description"]).toEqual(
+            "updated action description",
+        );
 
+        // Controleren dat de waarde effectief bewerkt is in de databank
         const resultGet = await session
             .get("/action/" + resultAdd.body["id"])
             .set("Cookie", [cookies]);
         expect(resultGet.status).toEqual(200);
-        expect(resultGet.body).toEqual(resultAdd.body);
+        expect(resultGet.body).toEqual(resultUpdate.body);
 
-        // Uiteindelijk moet de nieuwe actie terug verwijderd worden.
+        // Uiteindelijk moet de nieuwe waarde terug verwijderd worden.
         const resultDelete = await session
             .delete("/action/" + resultAdd.body["id"])
             .set("Cookie", [cookies]);
