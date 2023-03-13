d60848910864e3e4b275379a20ba43683d66c9f8
Arnout_Allaert <arnout.allaert@ugent.be>
2023-03-13 15:07:34 +0100

    afvaltypes, checkbox + margins

--- web/src/views/SchedulingScreenStudents.vue
+++ web/src/views/SchedulingScreenStudents.vue
@@ -1,26 +1,45 @@
 <template>
-  <v-list>
+  <v-list class="list">
     <v-list-item v-for="day in sorted" :key="day.name">
       <v-card class="dayCard">
         <v-card-item>
           <v-card-title>{{ day.name }}</v-card-title>
         </v-card-item>
+        <v-divider></v-divider>
         <v-list>
           <v-list-item v-for="building in day.buildings" :key="building.name">
-            <v-card-item class="buildingCard">
-              <v-card-title>
-                <router-link to="/dashboard">{{
-                  building.name
-                }}</router-link></v-card-title
-              >
-              <v-card-subtitle>{{ building.address }} </v-card-subtitle>
-              <v-card-title
-                >{{ building.deadline.getHours() }}:{{
-                  ("0" + building.deadline.getUTCMinutes()).slice(-2)
-                }}</v-card-title
-              >
-              {{ building.info }}
-            </v-card-item>
+            <div class="d-flex">
+              <v-sheet>
+                <v-card-item class="buildingCard">
+                  <v-card-title>
+                    <router-link to="/dashboard">{{
+                      building.name
+                    }}</router-link></v-card-title
+                  >
+                  <v-card-subtitle>{{ building.address }} </v-card-subtitle>
+                  <v-card-title
+                    >{{ building.deadline.getHours() }}:{{
+                      ("0" + building.deadline.getUTCMinutes()).slice(-2)
+                    }}</v-card-title
+                  >
+                  <div>
+                    <v-sheet class="d-flex">
+                      <v-sheet
+                        v-for="gb in building.garbage"
+                        :key="gb"
+                        class="flexbox"
+                      >
+                        {{ gb }}
+                      </v-sheet>
+                    </v-sheet>
+                  </div>
+                  {{ building.info }}
+                </v-card-item>
+              </v-sheet>
+              <v-sheet>
+                <v-checkbox></v-checkbox>
+              </v-sheet>
+            </div>
             <v-divider></v-divider>
           </v-list-item>
         </v-list>
@@ -44,24 +63,28 @@ export default {
               address: "coolestraat 69",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 19, 0),
+              garbage: ["PMD", "REST"],
             },
             {
               name: "gebouw2",
               address: "anderestraat 420",
               info: "wegens werken dient de achteringang genomen te worden",
               deadline: new Date(2023, 0o2, 0o6, 20, 30),
+              garbage: ["GLAS"],
             },
             {
               name: "gebouw3",
               address: "teamstraat 1",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 21, 0),
+              garbage: ["REST"],
             },
             {
               name: "gebouw4",
               address: "geeninspiratiestraat 13",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 19, 30),
+              garbage: ["PMD", "PAPIER"],
             },
           ],
         },
@@ -73,24 +96,28 @@ export default {
               address: "coolestraat 69",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 19, 30),
+              garbage: ["PMD", "REST", "GLAS"],
             },
             {
               name: "gebouw2",
               address: "anderestraat 420",
               info: "wegens werken dient de achteringang genomen te worden",
               deadline: new Date(2023, 0o2, 0o6, 19, 0),
+              garbage: ["PMD", "REST"],
             },
             {
               name: "gebouw3",
               address: "teamstraat 1",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 22, 0),
+              garbage: ["REST"],
             },
             {
               name: "gebouw4",
               address: "geeninspiratiestraat 13",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 19, 0),
+              garbage: ["PAPIER"],
             },
           ],
         },
@@ -102,24 +129,28 @@ export default {
               address: "coolestraat 69",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 21, 30),
+              garbage: ["PMD", "PAPIER"],
             },
             {
               name: "gebouw2",
               address: "anderestraat 420",
               info: "wegens werken dient de achteringang genomen te worden",
               deadline: new Date(2023, 0o2, 0o6, 19, 45),
+              garbage: ["REST", "PAPIER"],
             },
             {
               name: "gebouw3",
               address: "teamstraat 1",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 20, 0),
+              garbage: ["PMD", "REST"],
             },
             {
               name: "gebouw4",
               address: "geeninspiratiestraat 13",
               info: "meer info/opmerkingen",
               deadline: new Date(2023, 0o2, 0o6, 20, 30),
+              garbage: ["PMD", "REST", "GLAS", "PAPIER"],
             },
           ],
         },
@@ -144,6 +175,26 @@ export default {
 <style scoped lang="scss">
 @import "src/assets/styles/base";
 
+.list {
+  margin-top: -35px;
+  margin-left: -35px;
+  margin-right: -35px;
+}
+
+.flexbox {
+  margin-right: 15px;
+}
+
+.dayCard {
+  border: 1px solid lightgray;
+}
+
+.buildingCard {
+  width: 250px;
+}
+
+/*
+style can still be changed
 .dayCard {
   background-color: $accent;
 }
@@ -151,4 +202,5 @@ export default {
 .buildingCard:hover {
   background-color: $accent;
 }
+*/
 </style>
