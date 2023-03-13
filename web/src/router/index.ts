057bfd0151135fed224120a7537639b6a761fcfe
Ludovic <ludovic.verstraete@ugent.be>
2023-03-13 21:38:24 +0100

    lint

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginScreen from "@/views/LoginScreen.vue";
 import MainLayout from "@/layouts/MainLayout.vue";
-import HelloWorld from "@/views/HelloWorld.vue";
-import PhotoMaker from "@/components/PhotoMaker.vue";
 
 const routes = [
   {
