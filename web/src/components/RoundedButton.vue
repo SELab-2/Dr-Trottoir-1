98173e69e8a70bc59300e3954f54c2e8a8e4b5a4
Jens Pots <jens@jenspots.com>
2023-03-14 23:39:28 +0100

    feat: reworked building page

---
+++ web/src/components/RoundedButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="button">
+    <v-icon color="#333333" :icon="props.icon" size="small"></v-icon>
+    <p>{{ props.value }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#button {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  padding: 5px 10px;
+  border-radius: 50px;
+  background: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+  border: 1.5px solid #e0e0e0;
+  width: fit-content;
+  height: fit-content;
+}
+</style>
+
+<script lang="ts" setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+});
+</script>
