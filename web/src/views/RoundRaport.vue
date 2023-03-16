ebb6db0c80e4457bdcee0b77632245402a9e6614
Ludovic <ludovic.verstraete@ugent.be>
2023-03-16 12:02:38 +0100

    lint

--- web/src/views/RoundRaport.vue
+++ web/src/views/RoundRaport.vue
@@ -67,9 +67,7 @@
       </v-container>
       <v-card-actions class="d-flex">
         <v-spacer></v-spacer>
-        <v-btn prepend-icon="mdi-close" color="error"
-          >Niet afsluiten</v-btn
-        >
+        <v-btn prepend-icon="mdi-close" color="error">Niet afsluiten</v-btn>
         <v-btn prepend-icon="mdi-check" color="primary">Afsluiten</v-btn>
       </v-card-actions>
     </v-card>
