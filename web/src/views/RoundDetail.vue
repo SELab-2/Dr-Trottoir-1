873fa142e38436c7ac3633d8bbc483774f82938c
arne <vanheule.arne@ugent.be>
2023-03-16 03:47:41 +0100

    feat: Added visual interface but still width bug

--- web/src/views/RoundDetail.vue
+++ web/src/views/RoundDetail.vue
@@ -1,74 +1,94 @@
 <template>
+<v-container fluid>
   <v-card>
     <v-card-title>
       {{ mockround.name }}
     </v-card-title>
     <v-card-subtitle>
-      <v-icon icon="mdi-account">
-      </v-icon>{{ mockround.student }}
+      <v-icon icon="mdi-account"> </v-icon>{{ mockround.student }}
     </v-card-subtitle>
-  <v-timeline side="end">
-    <v-timeline-item dot-color="green" icon="mdi-check">
-    <v-card>
-        <v-card-title>
-          Start {{ mockround.start }}
-        </v-card-title>
-      </v-card>
-    </v-timeline-item>
-    <v-timeline-item
-      v-for="(building, id) in mockround.buildings"
-      :key="id"
-      :dot-color="mockdata[id].color"
-      :size="mockdata[id].color != 'red'? 'default' : 'small'"
-      :icon="mockdata[id].icon"
-    >
-    <v-card>
-      <div class="d-flex mb-6 align-center">
-        <v-card-title class="ma-2 pa-2 me-auto">
-          {{ building.name }}
-        </v-card-title>
-        <v-card-title class="ma-2 pa-2">
-          {{ cleanup_time_data(id) }}
-        </v-card-title>
-      </div>
-        
-        <v-card-text class="bg-white text--primary">
-          <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
-          <v-btn
-            color="blue"
-            variant="outlined"
-          >
-            Button
-          </v-btn>
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-  </v-timeline></v-card>
+    <v-container class="d-flex" fluid>
+    <v-timeline truncate-line="both" side="end">
+      <v-timeline-item dot-color="green" icon="mdi-check">
+        <v-card>
+          <v-card-title> Start {{ mockround.start }} </v-card-title>
+        </v-card>
+      </v-timeline-item>
+      <v-timeline-item width="100%"
+        v-for="(building, id) in mockround.buildings"
+        :key="id"
+        :dot-color="mockdata[id].color"
+        :size="mockdata[id].color != 'red' ? 'default' : 'small'"
+        :icon="mockdata[id].icon"
+        icon-color="white"
+      >
+        <v-card
+          width="100%"
+          :title="building.name" 
+          :subtitle="building.address"
+        >
+          <template v-slot:append>
+            <v-card-title>{{ cleanup_time_data(id) }}</v-card-title>
+          </template>
+          <v-chip
+          prepend-icon="mdi-camera"
+          label
+          color="success"
+          class="pa-2 ma-2"
+          v-if="id < 2"
+        >
+          {{ building.amount_of_pics }} foto's geupload
+        </v-chip>
+        <v-chip
+          prepend-icon="mdi-comment"
+          label
+          color="red"
+          class="pa-2 ma-2"
+          v-if="building.comments"
+        >
+          Opmerkingen beschikbaar
+        </v-chip>
+        </v-card>
+      </v-timeline-item>
+      <v-timeline-item dot-color="red" icon="mdi-close" size="small">
+        <v-card class="mx-4 mt-4">
+          <v-card-title> End {{ mockround.start }} </v-card-title>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
+  </v-card>
+</v-container>
 </template>
 
 <script lang="ts" setup>
 import Round from "@/components/models/Round";
 
 //TODO remove after mockpresentation
-const mockdata = [{time_start : '16:10', time_end: '16:12', color : 'green', icon : 'mdi-check'}, 
-                  {time_start : '16:15', time_end: '', color : 'orange', icon : 'mdi-account-clock-outline'},
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'},
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'},
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'}, 
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'},
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'},
-                  {time_start : '', time_end: '', color : 'red', icon : 'mdi-office-building'}]
+const mockdata = [
+  { time_start: "16:10", time_end: "16:12", color: "green", icon: "mdi-check" },
+  {
+    time_start: "16:15",
+    time_end: "",
+    color: "orange",
+    icon: "mdi-account-clock",
+  },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+  { time_start: "", time_end: "", color: "red", icon: "mdi-office-building" },
+];
 
-function cleanup_time_data(id : number){
-  const building = mockdata[id]
+function cleanup_time_data(id: number) {
+  const building = mockdata[id];
   if (building.time_end) {
-    return `${building.time_start} - ${building.time_end}`
+    return `${building.time_start} - ${building.time_end}`;
   } else {
-    return `${building.time_start}`
+    return `${building.time_start}`;
   }
-
 }
-
 
 const mockround: Round = {
   name: "Vrijdagmarkt",
@@ -83,8 +103,8 @@ const mockround: Round = {
       name: "Garcia",
       address: "Bruges, Belgium",
       deltatime: "15 min",
-      comments: false,
-      amount_of_pics: 3,
+      comments: true,
+      amount_of_pics: 5,
     },
     {
       name: "Miller",
