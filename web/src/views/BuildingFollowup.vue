e47f171914cd98f7591a7b114a1ceb93f33c14af
arne <vanheule.arne@ugent.be>
2023-03-16 20:59:43 +0100

    fix: new comment

--- web/src/views/BuildingFollowup.vue
+++ web/src/views/BuildingFollowup.vue
@@ -111,8 +111,8 @@ const mockbuildingdata: any[] = [
     student: "Mats Van Belle",
     comments: [
       {
-        title: "Ver",
-        comment: "Zeer ver van de andere gebouwen in de ronde",
+        title: "Kapotte deur",
+        comment: "De deur in de berging is kapot",
       },
     ],
   },
