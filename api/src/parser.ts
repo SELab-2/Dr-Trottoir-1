a694f7463848028b2c7cc5437ace1f209280e583
Jozef Jankaj <jozef.jankaj@ugent.be>
2023-03-26 18:28:09 +0200

    Implement Parser.order

--- api/src/parser.ts
+++ api/src/parser.ts
@@ -79,4 +79,47 @@ export class Parser {
 
         return result;
     }
+
+    /***
+     * Construct a list of objects for Prisma to order on
+     * List is constructed pair-wise:
+     *      Parser.order([a,b,c], [1,2,3]) will result in [{a: 1}, {b: 2}, {c: 3}]
+     * Prisma orders front to back, so fields that should be sorted on first should be at the front of the list
+     * @param sort_fields fields to sort on
+     * @param order_fields order the fields are supposed to have
+     * @return a list of objects for Prisma to use in orderBy query option
+     */
+    static order(
+        sort_fields: string | undefined,
+        order_fields: string | undefined,
+    ): object[] {
+        const allowedOrderValues = ["desc", "asc"]; // these are the values allowed by Prisma
+        const sort_fields_separated = sort_fields?.split(",");
+        const order_fields_separated = order_fields?.split(",");
+
+        // if there's no fields to sort on, return empty list
+        // In this case, Prisma will return the elements as recorded in the database
+        if (sort_fields_separated === undefined) {
+            return [];
+        }
+
+        const out = [];
+        for (let i = 0; i < sort_fields_separated.length; i++) {
+            const field = sort_fields_separated[i];
+            let sortOrder;
+            if (order_fields_separated === undefined) {
+                sortOrder = "desc"; // default value
+            } else {
+                sortOrder = order_fields_separated[i];
+                // if an invalid value is given, skip that field
+                if (!allowedOrderValues.includes(sortOrder)) {
+                    continue;
+                }
+            }
+
+            out.push({ [field]: sortOrder });
+        }
+
+        return out;
+    }
 }
