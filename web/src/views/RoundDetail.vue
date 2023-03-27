88161199b62663af975fff15d3e3b8718c226e4e
brent <brent-matthys@outlook.com>
2023-03-27 15:35:56 +0200

    Upgrade student indication

--- web/src/views/RoundDetail.vue
+++ web/src/views/RoundDetail.vue
@@ -1,77 +1,91 @@
 <template>
-  <v-container fluid>
-    <v-card :title="mockround.name" :subtitle="date" variant="flat">
-      <v-card-subtitle>
-        <v-icon icon="mdi-account"> </v-icon>{{ mockround.student }}
-      </v-card-subtitle>
-      <v-container fluid>
-        <v-timeline truncate-line="both" side="end" density="compact">
-          <v-timeline-item dot-color="green" icon="mdi-check">
-            <v-card>
-              <v-card-title> Start {{ mockround.start }} </v-card-title>
-            </v-card>
-          </v-timeline-item>
-          <v-timeline-item
-            width="100%"
-            v-for="(building, id) in mockround.buildings"
-            :key="id"
-            :dot-color="mockdata[id].color"
-            :size="mockdata[id].color != 'red' ? 'default' : 'small'"
-            :icon="mockdata[id].icon"
-            icon-color="white"
+
+  <v-card :title="mockround.name" :subtitle="date" variant="flat">
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <v-btn
+            v-bind="props"
+            class="text-body-2 mb-1 ml-3"
+            :variant="isHovering ? 'elevated': 'text'"
+            :color="isHovering ? 'primary': ''"
+            to="/account/0/false"
           >
-            <router-link to="/gebouw/3">
-              <v-card
-                width="100%"
-                :title="building.name"
-                :subtitle="building.address"
-              >
-                <template v-slot:append>
-                  <v-card-title>{{ cleanup_time_data(id) }}</v-card-title>
-                </template>
-                <v-chip
-                  prepend-icon="mdi-camera"
-                  label
-                  color="success"
-                  class="pa-2 ma-2"
-                  v-if="id < 2"
-                >
-                  {{ building.amount_of_pics }} foto's geupload
-                </v-chip>
-                <v-chip
-                  prepend-icon="mdi-comment"
-                  label
-                  color="red"
-                  class="pa-2 ma-2"
-                  v-if="building.comments"
-                >
-                  Opmerkingen beschikbaar
-                </v-chip>
-              </v-card>
-            </router-link>
-          </v-timeline-item>
-          <v-timeline-item dot-color="red" icon="mdi-close" size="small">
-            <v-card>
-              <v-card-title> Einde {{ mockround.start }} </v-card-title>
-            </v-card>
-          </v-timeline-item>
-        </v-timeline>
-      </v-container>
-      <v-card-actions class="d-flex">
-        <v-spacer></v-spacer>
-        <v-btn
-          to="/dashboard/rondes/rapport"
-          prepend-icon="mdi-check"
-          color="primary"
-          >Ronde stoppen</v-btn
+            <template v-slot:prepend>
+              <Avatar :name="mockround.student" size="x-small"/>
+            </template>
+            {{ mockround.student }}    
+          </v-btn>
+        </template>
+      </v-hover>
+    <v-container fluid>
+      <v-timeline truncate-line="both" side="end" density="compact">
+        <v-timeline-item dot-color="green" icon="mdi-check">
+          <v-card>
+            <v-card-title> Start {{ mockround.start }} </v-card-title>
+          </v-card>
+        </v-timeline-item>
+        <v-timeline-item
+          width="100%"
+          v-for="(building, id) in mockround.buildings"
+          :key="id"
+          :dot-color="mockdata[id].color"
+          :size="mockdata[id].color != 'red' ? 'default' : 'small'"
+          :icon="mockdata[id].icon"
+          icon-color="white"
         >
-      </v-card-actions>
-    </v-card>
-  </v-container>
+          <router-link to="/gebouw/3">
+            <v-card
+              width="100%"
+              :title="building.name"
+              :subtitle="building.address"
+            >
+              <template v-slot:append>
+                <v-card-title>{{ cleanup_time_data(id) }}</v-card-title>
+              </template>
+              <v-chip
+                prepend-icon="mdi-camera"
+                label
+                color="success"
+                class="pa-2 ma-2"
+                v-if="id < 2"
+              >
+                {{ building.amount_of_pics }} foto's geupload
+              </v-chip>
+              <v-chip
+                prepend-icon="mdi-comment"
+                label
+                color="red"
+                class="pa-2 ma-2"
+                v-if="building.comments"
+              >
+                Opmerkingen beschikbaar
+              </v-chip>
+            </v-card>
+          </router-link>
+        </v-timeline-item>
+        <v-timeline-item dot-color="red" icon="mdi-close" size="small">
+          <v-card>
+            <v-card-title> Einde {{ mockround.start }} </v-card-title>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-container>
+    <v-card-actions class="d-flex">
+      <v-spacer></v-spacer>
+      <v-btn
+        to="/dashboard/rondes/rapport"
+        prepend-icon="mdi-check"
+        color="primary"
+        >Ronde stoppen</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+
 </template>
 
 <script lang="ts" setup>
 import Round from "@/components/models/Round";
+import Avatar from "@/components/Avatar.vue";
 
 const date = "13/03/2023";
 
