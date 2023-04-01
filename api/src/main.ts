97cfeb6a05eda7120a1dee694c4a990f8164c1c5
Jens Pots <jens@jenspots.com>
2023-04-01 16:23:29 +0200

    refactor(api): do not have resident route in different class

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,6 @@ import { AuthRouting } from "./routes/auth";
 import { ActionRouting } from "./routes/action";
 import { SyndicusRouting } from "./routes/syndicus";
 import { RoundRouting } from "./routes/round";
-import { BuildingResidentRouting } from "./routes/buildingResident";
 import passport from "passport";
 import crypto from "crypto";
 import session from "express-session";
@@ -70,7 +69,6 @@ app.use("/action", new ActionRouting().toRouter());
 app.use("/syndicus", new SyndicusRouting().toRouter());
 app.use("/round", new RoundRouting().toRouter());
 app.use("/progress", new ProgressRouting().toRouter());
-app.use("/buildingResident", new BuildingResidentRouting().toRouter);
 
 // Finally, an error handler
 app.use(ErrorHandler.handle);
--- api/src/routes/building.ts
+++ api/src/routes/building.ts
@@ -4,6 +4,8 @@ import { CustomRequest, includeUser, Routing } from "./routing";
 import { Auth } from "../auth/auth";
 import { Parser } from "../parser";
 import { Prisma } from "@selab-2/groep-1-orm";
+import { APIError } from "../errors/api_error";
+import { APIErrorCode } from "../errors/api_error_code";
 
 export class BuildingRouting extends Routing {
     private static selects: Prisma.BuildingSelect = {
@@ -98,5 +100,69 @@ export class BuildingRouting extends Routing {
         }
 
         return res.status(200).json({});
+    }
+
+    async resident(req: CustomRequest, res: express.Response) {
+        // Default hash is the empty string, which doesn't match anything.
+        const hash = Parser.string(req.params["hash"], "");
+
+        const result = await prisma.building.findUniqueOrThrow({
+            where: {
+                hash: hash,
+            },
+            select: {
+                id: true,
+                name: true,
+                ivago_id: true,
+                deleted: true,
+                hash: false,
+                address: true,
+                syndicus: {
+                    include: {
+                        user: includeUser(false),
+                    },
+                },
+                manual: true,
+                images: {
+                    include: {
+                        image: true,
+                    },
+                },
+                garbage: {
+                    include: {
+                        action: true,
+                    },
+                },
+                progress: {
+                    include: {
+                        schedule: {
+                            include: {
+                                user: true,
+                                round: true,
+                            },
+                        },
+                        images: {
+                            include: {
+                                image: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+        // Residents may not see their building if it has been soft deleted.
+        if (result.deleted) {
+            throw new APIError(APIErrorCode.NOT_FOUND);
+        }
+
+        return res.json(result);
+    }
+
+
+    toRouter(): express.Router {
+        const router = super.toRouter();
+        router.get("/resident/:hash", this.resident);
+        return router
     }
 }
