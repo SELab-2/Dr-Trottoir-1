77fb0dfd97d5ca6c04a584901210c2b0c1369254
arne <vanheule.arne@ugent.be>
2023-04-03 20:29:12 +0200

    chore: useless v-card removed

--- web/src/views/ContactSyndicus.vue
+++ web/src/views/ContactSyndicus.vue
@@ -1,90 +1,88 @@
 <template>
-  <v-card variant="flat">
-    <v-card variant="flat" class="mx-4">
-      <v-row class="py-0 my-0">
-        <v-col
-          cols="1"
-          style="min-width: 100px; max-width: 100%"
-          class="flex-grow-1 flex-shrink-0 py-0 my-0"
-        >
-          <v-select
-            label="Syndicus"
-            v-model="syndicus_data"
-            :items="mock_syndici_data"
-            item-value="syndicus"
-            item-title="syndicus"
-            return-object
-            prepend-inner-icon="mdi-account"
-            variant="solo"
-          >
-          </v-select>
-        </v-col> </v-row
-      ><v-row class="py-0 my-0">
-        <v-col
-          cols="1"
-          style="min-width: 100px; max-width: 100%"
-          class="flex-grow-1 flex-shrink-0 py-0 my-0"
-        >
-          <v-select
-            label="Gebouw"
-            v-model="building"
-            :items="syndicus_data?.buildings"
-            return-object
-            prepend-inner-icon="mdi-office-building"
-            variant="solo"
-          >
-          </v-select>
-        </v-col>
-        <v-col cols="3" class="flex-grow-0 flex-shrink-0 py-0 my-0">
-          <v-select
-            label="Sjabloon"
-            v-model="selected_template"
-            :items="templates"
-            item-value="name"
-            item-title="name"
-            return-object
-            prepend-inner-icon="mdi-list-box"
-            variant="solo"
-          >
-          </v-select>
-        </v-col>
-      </v-row>
-      <v-row class="py-0 my-0">
-        <v-text-field
-          prepend-inner-icon="mdi-text-short"
+  <v-card variant="flat" class="mx-4">
+    <v-row class="py-0 my-0">
+      <v-col
+        cols="1"
+        style="min-width: 100px; max-width: 100%"
+        class="flex-grow-1 flex-shrink-0 py-0 my-0"
+      >
+        <v-select
+          label="Syndicus"
+          v-model="syndicus_data"
+          :items="mock_syndici_data"
+          item-value="syndicus"
+          item-title="syndicus"
+          return-object
+          prepend-inner-icon="mdi-account"
           variant="solo"
-          label="Onderwerp"
-          :model-value="selected_template.subject"
         >
-        </v-text-field>
-      </v-row>
-      <v-row class="py-0 my-0"
-        ><v-col
-          cols="1"
-          style="min-width: 100px; max-width: 100%"
-          class="flex-grow-1 flex-shrink-0 py-0 my-0"
+        </v-select>
+      </v-col> </v-row
+    ><v-row class="py-0 my-0">
+      <v-col
+        cols="1"
+        style="min-width: 100px; max-width: 100%"
+        class="flex-grow-1 flex-shrink-0 py-0 my-0"
+      >
+        <v-select
+          label="Gebouw"
+          v-model="building"
+          :items="syndicus_data?.buildings"
+          return-object
+          prepend-inner-icon="mdi-office-building"
+          variant="solo"
         >
-          <v-textarea
-            prepend-inner-icon="mdi-text-long"
-            rows="20"
-            label="Uitleg"
-            variant="solo"
-            :model-value="selected_template.content"
-          ></v-textarea
-        ></v-col>
-        <v-col cols="3" class="flex-grow-0 flex-shrink-0 py-0 my-0">
-          <v-img
-            src="../assets/images/defaultImage.png"
-            cover
-            height="96%"
-          ></v-img
-        ></v-col>
-      </v-row>
-      <v-card-actions class="d-flex">
-        <v-spacer></v-spacer>
-        <v-btn prepend-icon="mdi-send" color="primary">Versturen</v-btn>
-      </v-card-actions>
-    </v-card>
+        </v-select>
+      </v-col>
+      <v-col cols="3" class="flex-grow-0 flex-shrink-0 py-0 my-0">
+        <v-select
+          label="Sjabloon"
+          v-model="selected_template"
+          :items="templates"
+          item-value="name"
+          item-title="name"
+          return-object
+          prepend-inner-icon="mdi-list-box"
+          variant="solo"
+        >
+        </v-select>
+      </v-col>
+    </v-row>
+    <v-row class="py-0 my-0">
+      <v-text-field
+        prepend-inner-icon="mdi-text-short"
+        variant="solo"
+        label="Onderwerp"
+        :model-value="selected_template.subject"
+      >
+      </v-text-field>
+    </v-row>
+    <v-row class="py-0 my-0"
+      ><v-col
+        cols="1"
+        style="min-width: 100px; max-width: 100%"
+        class="flex-grow-1 flex-shrink-0 py-0 my-0"
+      >
+        <v-textarea
+          prepend-inner-icon="mdi-text-long"
+          rows="20"
+          label="Uitleg"
+          variant="solo"
+          :model-value="selected_template.content"
+        ></v-textarea
+      ></v-col>
+      <v-col cols="3" class="flex-grow-0 flex-shrink-0 py-0 my-0">
+        <v-img
+          src="../assets/images/defaultImage.png"
+          cover
+          height="96%"
+        ></v-img
+      ></v-col>
+    </v-row>
+    <v-card-actions class="d-flex">
+      <v-spacer></v-spacer>
+      <v-btn prepend-icon="mdi-send" color="primary">Versturen</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
