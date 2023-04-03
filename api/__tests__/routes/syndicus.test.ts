60302f199af2c7b23c0aee2598cfe2a49a9a7963
Janne Cools <janne.cools@ugent.be>
2023-04-03 18:31:36 +0200

    test: toevoeging testen voor route Syndicus + aanpassing route User

--- api/__tests__/routes/syndicus.test.ts
+++ api/__tests__/routes/syndicus.test.ts
@@ -4,6 +4,31 @@ import request from "supertest";
 // @ts-ignore
 import supertest from "supertest";
 
+// User die toegevoegd zal worden om mee te werken bij de testen
+const userToCreate = {
+    email: "admin@email.com",
+    first_name: "admin",
+    last_name: "familyname",
+    date_added: "2020-01-01T00:00:00.000Z",
+    last_login: "2020-01-01T00:00:00.000Z",
+    phone: "number",
+    address: {
+        create: {
+            city: "Gent",
+            latitude: 100.0,
+            longitude: 100.0,
+            number: 1,
+            street: "street",
+            zip_code: 1000,
+        },
+    },
+    address_id: undefined,
+    student: false,
+    super_student: false,
+    admin: true,
+    password: "adminPassword",
+};
+
 // Syndicus die toegevoegd zal worden om mee te werken bij de testen
 const syndicusToCreate = {
     user_id: undefined,
@@ -35,16 +60,17 @@ async function prepareSession(): Promise<[supertest.SuperTest<any>, string]> {
     // Deze constante zorgt ervoor dat de ingelogde gebruiker behouden blijft en dus autorisatie heeft.
     const cookies = headers["set-cookie"].pop().split(";")[0];
 
-    // Een willekeurige gebruiker nemen om te gebruiken voor het maken van de syndicus
-    res = await session.get("/user").set("Cookie", [cookies]);
-    expect(res.status).toEqual(200);
-    const users = res.body;
-    expect(users.length).toBeGreaterThan(0);
-    const user = users[0];
+    // Een gebruiker toevoegen om te gebruiken voor het maken van de syndicus
+    res = await session
+        .post("/user")
+        .send(userToCreate)
+        .set("Cookie", [cookies]);
+    expect(res.status).toEqual(201);
+    const user = res.body;
     syndicusToCreate.user_id = user.id;
     syndicus.user_id = user.id;
     delete user.regions;
-    syndicus.user =user;
+    syndicus.user = user;
 
     // Nieuwe schedule toevoegen die gebruikt wordt bij de testen
     res = await session
@@ -63,10 +89,19 @@ async function closeSession(
     session: supertest.SuperTest<any>,
     cookies: string,
 ) {
-    const res = await session
+    // Toegevoegde user terug verwijderen
+    let res = await session
+        .delete("/user/" + syndicus.user_id)
+        .send({hardDelete: true})
+        .set("Cookie", [cookies]);
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({});
+
+    res = await session
         .delete("/syndicus/" + syndicus.id)
         .set("Cookie", [cookies]);
     expect(res.status).toEqual(200);
+    expect(res.body).toEqual({});
 }
 
 describe("Successful tests", () => {
@@ -89,6 +124,28 @@ describe("Successful tests", () => {
             .set("Cookie", [cookies]);
         expect(res.status).toEqual(200);
         expect(res.body).toEqual(syndicus);
+    });
+
+    test("Test updating existing syndicus", async () => {
+        let res = await session
+            .get("/user")
+            .set("Cookie", [cookies]);
+        expect(res.status).toEqual(200)
+        const users = res.body;
+        expect(users.length).toBeGreaterThan(0);
+        const user = users[0];
+
+        const updatedSyndicus = {
+            id: syndicus.id,
+            user_id: user.id,
+        };
+
+        res = await session
+            .patch("/syndicus/" + syndicus.id)
+            .send({user_id: user.id})
+            .set("Cookie", [cookies]);
+        expect(res.status).toEqual(200);
+        expect(res.body).toEqual(updatedSyndicus);
     })
 })
 
