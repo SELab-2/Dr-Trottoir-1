d5dc798d29cfcb5ccdc42f0ac5b9064d092f9e23
Ludovic <ludovic.verstraete@ugent.be>
2023-04-06 12:10:09 +0200

    fix: api test

--- api/__tests__/routes/building.test.ts
+++ api/__tests__/routes/building.test.ts
@@ -23,6 +23,7 @@ const building = {
     address: undefined,
     manual: {
         id: 1,
+        name: "dummy.pdf",
         path: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
         location: "EXTERNAL",
     },
