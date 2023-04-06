bf2fadc0dd92cab6131361dcb3285995ca1daac2
brent <brent-matthys@outlook.com>
2023-04-06 18:19:48 +0200

    Add HFill components

---
+++ web/src/layouts/hfill/HFillWrapper.vue
@@ -0,0 +1,17 @@
+<!--
+    Put whitespace left and right of the content,
+    But only when the screen is broad enough.
+-->
+<template>
+  <v-row>
+    <HFill />
+    <v-col style="min-width: 65%" :class="margin">
+      <slot></slot>
+    </v-col>
+    <HFill />
+  </v-row>
+</template>
+<script setup lang="ts">
+import HFill from "@/components/HFill.vue";
+defineProps({ margin: { type: String, default: "ma-3" } });
+</script>
