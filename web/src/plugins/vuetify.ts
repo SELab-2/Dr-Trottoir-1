80be50db5f95af9ac57b00e3a59cda652a942956
brent <brent-matthys@outlook.com>
2023-04-07 08:20:07 +0200

    set grey theme

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -9,11 +9,31 @@ import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
 // Composables
-import { createVuetify } from "vuetify";
+import { createVuetify, ThemeDefinition } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const drTrottoirLight: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#F5F5F5',
+    error: '#B00020',
+    success: '#4CAF50',
+    warning: '#FF9800',
+    primary: '#1867C0',
+    secondary: '#009688',    
+  }
+}
+
+
 export default createVuetify({
   theme: {
+    defaultTheme: 'drTrottoirLight',
+    themes: {
+      drTrottoirLight,
+    }
+  }
+  /*theme: {
     themes: {
       light: {
         colors: {
@@ -22,5 +42,5 @@ export default createVuetify({
         },
       },
     },
-  },
+  },*/
 });
