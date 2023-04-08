d0e0051cfa80058125299dd0cda26af3433053f9
Ludovic <ludovic.verstraete@ugent.be>
2023-04-08 20:08:22 +0200

    feat: start new concept

--- web/src/views/building/GarbageMaker.vue
+++ web/src/views/building/GarbageMaker.vue
@@ -1,228 +1,184 @@
 <template>
-  <div class="mx-4">
-    <v-card variant="flat">
+  <v-container>
+    <div v-for="(schedule, index) in schedules" :key="index">
       <v-row>
         <v-col>
           <v-select
-            variant="solo"
-            v-model="selectedBuilding"
-            :items="buildings"
-            label="Selecteer Gebouw"
+            v-model="schedule.garbageType"
+            :items="garbageTypes"
+            label="Vuilnis Type"
+            outlined
+            dense
           ></v-select>
         </v-col>
+
+        <v-col >
+          <v-select
+            v-model="schedule.action"
+            :items="actions"
+            label="Actie"
+            outlined
+            dense
+          ></v-select>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            v-model="schedule.startDate"
+            label="Start Datum"
+            type="date"
+            outlined
+            dense
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            v-model="schedule.endDate"
+            label="Einde Datum"
+            type="date"
+            outlined
+            dense
+          ></v-text-field>
+        </v-col>
+
         <v-col>
           <v-select
-            variant="solo"
-            v-model="selectedWeek"
-            :items="weeks"
-            label="Selecteer Week"
+            v-model="schedule.frequency"
+            :items="frequencies"
+            label="Frequenctie"
+            outlined
+            dense
           ></v-select>
         </v-col>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-            @click="show = !show"
-          ></v-btn>
-        </v-card-actions>
+
+        <v-col>
+          <v-select
+            v-model="schedule.day"
+            :items="days"
+            label="Dag"
+            outlined
+            dense
+          ></v-select>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            v-model="schedule.time"
+            label="Tijd"
+            type="time"
+            outlined
+            dense
+          ></v-text-field>
+        </v-col>
       </v-row>
-      <v-expand-transition>
-        <div v-show="show">
-          <v-row>
-            <v-col>
-              <v-select
-                variant="solo"
-                v-model="selectedDay"
-                :items="days"
-                label="Selecteer Dag"
-              ></v-select>
-            </v-col>
-            <v-col>
-              <v-select
-                variant="solo"
-                chips
-                v-model="selectedGarbage"
-                :items="garbageTypes"
-                label="Selecteer Vuilnis"
-                multiple
-              ></v-select>
-            </v-col>
-            <v-col>
-              <v-select
-                variant="solo"
-                chips
-                multiple
-                v-model="selectedAction"
-                :items="actions"
-                label="Selecteer Actie"
-              ></v-select>
-            </v-col>
-          </v-row>
-          <v-card-actions class="d-flex">
-            <v-spacer></v-spacer>
-            <v-btn
-              @click="addItem"
-              :disabled="
-                !(
-                  selectedBuilding &&
-                  selectedDay &&
-                  selectedWeek &&
-                  selectedGarbage.length !== 0 &&
-                  selectedAction.length !== 0
-                )
-              "
-              prepend-icon="mdi-plus"
-              color="primary"
-              >Toevoegen</v-btn
-            >
-          </v-card-actions>
-        </div>
-      </v-expand-transition>
-    </v-card>
+    </div>
 
-    <v-table
-      v-if="selectedBuilding && selectedWeek && buildingItems.length > 0"
-    >
-      <thead>
-        <tr>
-          <th class="text-left">Dag</th>
-          <th class="text-left">Vuilnis</th>
-          <th class="text-left">Actie</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in buildingItems" :key="item.building">
-          <td>{{ item.day }}</td>
-          <td>
-            <v-chip-group>
-              <v-chip v-for="garbage in item.garbage" :key="garbage">
-                {{ garbage }}
-              </v-chip>
-            </v-chip-group>
-          </td>
-          <td>
-            <v-chip-group>
-              <v-chip v-for="action in item.action" :key="action">
-                {{ action }}
-              </v-chip>
-            </v-chip-group>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
+    <v-row justify="center">
+        <v-btn color="primary" @click="addSchedule">+</v-btn>
+    </v-row>
 
-    <!-- && selectedBuilding.value&& selectedDay.value && selectedGarbage.value && selectedAction.value -->
-    <!--
-      <v-card v-if="selectedBuilding || selectedGarbage">
-        <v-card-title>Selected Building Info</v-card-title>
-        <v-data-table :headers="buildingHeaders" :items="buildingItems" hide-default-footer></v-data-table>
-        <v-btn v-if="buildingItems.length === 0" @click="addItem">Add</v-btn>
-      </v-card>
-      -->
-  </div>
+    <v-row>
+      <v-col cols="12">
+        <h4>Schedules:</h4>
+        <v-list>
+          <v-list-item
+            v-for="(schedule, index) in sortedSchedules"
+            :key="index"
+          >
+            <v-list-item-content>
+              <div class="d-flex align-center">
+                <div :style="{backgroundColor: getColor(schedule.garbageType), borderRadius: '50%', width: '20px', height: '20px'}"></div>
+                {{ schedule.day }} - {{ schedule.garbageType }} - {{
+                  schedule.action
+                }} - Start: {{ schedule.startDate }} - End: {{
+                  schedule.endDate
+                }} - {{ schedule.frequency }} - Time: {{ schedule.time }}
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-import { reactive, computed, ref } from "vue";
+import {ref, reactive, computed } from "vue";
 
-const show = ref(false);
-
-const buildings = ["Gebouw A", "Gebouw B", "Gebouw C", "Gebouw D", "Gebouw E"];
-const days = [
-  "Maandag",
-  "Dinsdag",
-  "Woensdag",
-  "Donderdag",
-  "Vrijdag",
-  "Zaterdag",
-  "Zondag",
-];
-const weeks = ["Oneven", "Even"];
-const garbageTypes = ["Vuilnis Type 1", "Vuilnis Type 2", "Vuilnis Type 3"];
-const actions = ["Actie 1", "Actie 2", "Actie 3"];
-const selectedBuilding = ref(null);
-const selectedDay = ref(null);
-const selectedWeek = ref(null);
-const selectedGarbage = ref([]);
-const selectedAction = ref([]);
-const buildingData: {
-  building: string;
-  garbage: string[];
+interface Schedule {
+  garbageType: string;
+  action: string;
+  startDate: string;
+  endDate: string;
+  frequency: string;
   day: string;
-  week: string;
-  action: string[];
-}[] = reactive([
+  time: string;
+}
+const schedules = reactive<Schedule[]>([
   {
-    building: "Gebouw A",
-    garbage: ["Vuilnis Type 1", "w"],
-    day: "Maandag",
-    week: "Oneven",
-    action: ["Actie 1"],
-  },
-  {
-    building: "Gebouw B",
-    garbage: ["Vuilnis Type 2"],
-    day: "Dinsdag",
-    week: "Even",
-    action: ["Actie 2"],
-  },
-  {
-    building: "Gebouw C",
-    garbage: ["Vuilnis Type 1"],
-    day: "Woensdag",
-    week: "Oneven",
-    action: ["Actie 3"],
-  },
-  {
-    building: "Gebouw D",
-    garbage: ["Vuilnis Type 3"],
-    day: "Donderdag",
-    week: "Even",
-    action: ["Actie 1"],
-  },
-  {
-    building: "Gebouw E",
-    garbage: ["Vuilnis Type 2"],
-    day: "Vrijdag",
-    week: "Oneven",
-    action: ["Actie 2"],
+    garbageType: "",
+    action: "",
+    startDate: "",
+    endDate: "",
+    frequency: "",
+    day: "",
+    time: "",
   },
 ]);
 
-const buildingItems = computed(() => {
-  if (selectedBuilding.value && selectedWeek.value) {
-    return buildingData.filter(
-      (item) =>
-        item.building === selectedBuilding.value &&
-        item.week === selectedWeek.value,
-    );
-  } else {
-    return [];
-  }
+const garbageTypes = [
+  "Vuilnis type 1",
+  "Vuilnis type 2",
+  "Vuilnis type 3",
+  "Vuilnis type 4",
+  "Vuilnis type 5",
+];
+const actions = ["Actie 1", "Actie 2-"];
+const frequencies = ["1", "2", "4"];
+const days = [
+  "M",
+  "D",
+  "W",
+  "D",
+  "V",
+  "Z",
+  "Z",
+];
+
+function addSchedule() {
+  schedules.push({
+    garbageType: "",
+    action: "",
+    startDate: "",
+    endDate: "",
+    frequency: "",
+    day: "",
+    time: "",
+  });
+}
+
+const sortedSchedules = computed(() => {
+  return schedules.slice().sort((a, b) => {
+    return a.day.localeCompare(b.day);
+  });
 });
 
-const addItem = () => {
-  if (
-    selectedBuilding.value &&
-    selectedDay.value &&
-    selectedWeek.value &&
-    selectedGarbage.value &&
-    selectedAction.value
-  ) {
-    buildingData.push({
-      building: selectedBuilding.value,
-      day: selectedDay.value,
-      week: selectedWeek.value,
-      garbage: selectedGarbage.value,
-      action: selectedAction.value,
-    });
-    selectedDay.value = null;
-    selectedGarbage.value = [];
-    selectedAction.value = [];
+function getColor(garbageType: string) {
+  switch (garbageType) {
+    case "Vuilnis type 1":
+      return "#F44336";
+    case "Vuilnis type 2":
+      return "#4CAF50";
+    case "Vuilnis type 3":
+      return "#FF9800";
+    case "Vuilnis type 4":
+      return "#2196F3";
+    case "Vuilnis type 5":
+      return "#9C27B0";
+    default:
+      return "#000000";
   }
-};
+}
 
-// const show = () => {
-//   add.value = !add.value;
-// };
 </script>
