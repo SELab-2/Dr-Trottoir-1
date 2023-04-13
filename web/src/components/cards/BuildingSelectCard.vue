87330ab5c0c83b7c0f2bcc3d1f4b48cc156e6446
arne <arne.vanheule@ugent.be>
2023-04-13 16:35:08 +0200

    feat: roundmaker in style with webpage"

---
+++ web/src/components/cards/BuildingSelectCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <BorderCard class="my-2">
+    <template v-slot:prepend>
+      <v-icon icon="mdi-office-building"></v-icon>
+    </template>
+    <template v-slot:title>{{ name }}</template
+    ><template v-slot:subtitle>{{ address }}</template>
+    <template v-slot:append>
+      <v-icon
+        @click="$emit('up')"
+        class="mx-1"
+        color="primary"
+        icon="mdi-chevron-up"
+      />
+      <v-icon
+        @click="$emit('down')"
+        class="mx-1"
+        color="primary"
+        icon="mdi-chevron-down"
+      />
+      <v-icon
+        @click="$emit('remove')"
+        class="mx-1"
+        color="error"
+        icon="mdi-close"
+      />
+    </template>
+    <v-expand-transition>
+    <v-table v-show="garbageinfo" class="mx-2 my-2" density="compact">
+      <thead>
+        <tr>
+          <th
+            v-for="day in ['MA', 'DI', 'WO', 'DO', 'VR', 'ZA', 'ZO']"
+            class="text-center"
+          >
+            {{ day }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in ['REST', 'GFT', 'PMD', 'PAPIER']" :key="item">
+          <td class="text-center" v-for="afval in 7">
+            <v-chip v-if="afval%2 == 0">{{ item }}</v-chip>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+    </v-expand-transition>
+  </BorderCard>
+</template>
+
+<script setup lang="ts">
+import BorderCard from "@/layouts/CardLayout.vue";
+import GarbageCard from "@/components/cards/GarbageCard.vue";
+
+const props = defineProps({
+  name: String,
+  address: String,
+  id: String,
+  garbageinfo : {
+    type: Boolean, 
+    default: true}
+});
+</script>
+
+<style scoped>
+td {
+  border-right: solid 1px rgb(216, 216, 216);
+  border-left: solid 1px rgb(216, 216, 216);
+}
+</style>
--- web/src/views/round/RoundMaker.vue
+++ web/src/views/round/RoundMaker.vue
@@ -3,104 +3,72 @@
     <v-col
       cols="1"
       style="min-width: 100px; max-width: 100%"
-      class="flex-grow-1 flex-shrink-0 py-0 my-0 ml-5"
-      ><v-card width="100%">
-        <!-- Header for the card title -->
-        <!-- TODO: remove the mockdata from this part -->
-        <v-row class="py-1 my-1">
-          <v-col
-            cols="1"
-            style="min-width: 100px; max-width: 100%"
-            class="flex-grow-1 flex-shrink-0 py-0 my-0 ml-5"
-            ><v-text-field label="Template naam" variant="solo" /></v-col
-          ><v-col
-            cols="3"
-            style="min-width: 100px; max-width: 100%"
-            class="flex-grow-0 flex-shrink-0 py-0 my-0 ml-5"
-            ><v-btn height="55px">Aanmaken</v-btn></v-col
-          >
-        </v-row>
-        <v-divider></v-divider>
-        <v-layout>
-          <v-list width="100%">
-            <v-card
-              v-for="(building, i) in round"
-              :key="i"
-              :value="building"
-              fluid
-              variant="flat"
-            >
-              <template v-slot:prepend>
-                <v-icon
-                  icon="mdi-chevron-up"
-                  @click="moveBuildingUp(i)"
-                ></v-icon>
-                <v-icon
-                  icon="mdi-chevron-down"
-                  @click="moveBuildingDown(i)"
-                ></v-icon>
-              </template>
-              <template v-slot:title>
-                <v-card-title>{{ building.name }}</v-card-title> </template
-              ><template v-slot:subtitle>
-                <v-card-subtitle>{{ building.adress }}</v-card-subtitle>
-              </template>
-              <template v-slot:append>
-                <v-icon
-                  icon="mdi-close"
-                  @click="deleteBuildingFromRound(i, building)"
-                ></v-icon>
-              </template>
-            </v-card>
-          </v-list>
-        </v-layout> </v-card
-    ></v-col>
+      class="flex-grow-1 flex-shrink-0 py-0 my-0"
+      ><border-card
+        class="mb-4"
+        title="Ronde aanmaken"
+        subtitle="De ronde zal in de volgorde van onderstaande lijst opgeslaan worden"
+      >
+        <template v-slot:append
+          ><v-switch
+            color="primary"
+            v-model="garbageinfo"
+            label="Toon afvalkalender"
+          ></v-switch
+        ></template>
+        <v-text-field class="ml-3 mr-5" label="Naam ronde" variant="solo" />
+        <v-card-actions class="d-flex align-center"
+          ><v-spacer></v-spacer
+          ><v-btn class="ml-3" prepend-icon="mdi-check"
+            >Ronde aanmaken</v-btn
+          ></v-card-actions
+        >
+      </border-card>
+      <building-select-card
+        v-for="(building, i) in round"
+        @remove="deleteBuildingFromRound(i, building)"
+        @up="moveBuildingUp(i)"
+        @down="moveBuildingDown(i)"
+        :name="building.name"
+        :address="building.adress"
+        :garbageinfo="garbageinfo"
+      ></building-select-card>
+    </v-col>
     <v-col
       cols="2"
       style="min-width: 100px; max-width: 100%"
-      class="flex-grow-1 flex-shrink-0 py-0 my-0 ml-5"
-      ><v-card width="100%">
-        <v-card-title>
-          <v-text-field
-            label="Zoeken op naam van bestaande gebouwen"
-            variant="solo"
-            v-model="searchquery"
-          ></v-text-field>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-layout>
-          <v-list width="100%">
-            <v-card
-              v-for="building in filterlist()"
-              :key="building.listid"
-              :value="building"
-              fluid
-              variant="flat"
-            >
-              <template v-slot:prepend>
-                <!-- Button for manipulating the building list -->
-                <v-icon
-                  icon="mdi-arrow-left-bold"
-                  @click="
-                    deleteBuildingFromAvailable(building.listid, building)
-                  "
-                ></v-icon>
-              </template>
-              <template v-slot:title>
-                <v-card-title>{{ building.name }}</v-card-title> </template
-              ><template v-slot:subtitle>
-                <v-card-subtitle>{{ building.adress }}</v-card-subtitle>
-              </template>
-            </v-card>
-          </v-list>
-        </v-layout>
-      </v-card></v-col
-    >
+      class="flex-grow-1 flex-shrink-0 py-0 ml-5"
+      ><border-card
+        title="Zoeken in gebouwen"
+        subtitle="Klik op een gebouw om deze toe te voegen aan de lijst"
+        class="mb-4 pt-3"
+        ><v-text-field
+          class="ml-3 mr-5 mt-3"
+          label="Naam van gebouw"
+          v-model="searchquery"
+          variant="solo"
+        />
+      </border-card>
+
+      <building-info-card
+        v-for="building in filterlist()"
+        :key="building.listid"
+        :name="building.name"
+        :address="building.adress"
+        @clicked="deleteBuildingFromAvailable(building.listid, building)"
+      >
+      </building-info-card>
+    </v-col>
   </v-row>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref, Ref } from "vue";
+import BorderCard from "@/layouts/CardLayout.vue";
+import BuildingSelectCard from "@/components/cards/BuildingSelectCard.vue";
+import BuildingInfoCard from "@/components/cards/BuildingInfoCard.vue";
+
+const garbageinfo: Ref<boolean> = ref(true);
 
 // The query that will be used to filter all available buildings
 const searchquery: Ref<string> = ref("");
