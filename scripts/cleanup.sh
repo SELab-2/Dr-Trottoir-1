8536a4ee4ac04b43c186344d5b68bf5bafa9f5d5
Jozef Jankaj <jozef.jankaj@ugent.be>
2023-04-14 14:21:53 +0200

    Update workflows with cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-# removes all docker artifacts
+PURGE=false
+
+while getopts "p" flag
+do
+    case "${flag}" in
+        p) PURGE=true;;
+        *) ;;
+    esac
+done
+
+# remove all docker artifacts
 docker system prune -af
 docker network prune -f
 docker volume prune -f
 docker container prune -f
 docker image prune -af
+
+if [ "$PURGE" = true ] ; then
+    echo "Purging runner files"
+    rm -rf ./*
+fi
