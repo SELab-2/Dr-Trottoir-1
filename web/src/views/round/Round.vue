f018ba5b1ab5447eca0e1d4f93471ffd73501c66
Jens Pots <jens@jenspots.com>
2023-04-18 01:04:45 +0200

    feat(web): schedule view refinements

--- web/src/views/round/Round.vue
+++ web/src/views/round/Round.vue
@@ -1,45 +1,45 @@
 <template>
   <HFillWrapper>
     <div class="space-y-8">
-      <div style="display: flex; gap: 8px; align-items: center">
-        <h1>Ronde Korenmarkt</h1>
-        <div class="flex-grow-1"></div>
-        <RoundedButton icon="mdi-pencil-plus" value="Kopieren"></RoundedButton>
-        <RoundedButton icon="mdi-pencil" value="Wijzigen"></RoundedButton>
-        <RoundedButton icon="mdi-calendar" value="Inplannen"></RoundedButton>
-      </div>
+      <h1>Ronde Korenmarkt</h1>
 
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+      <div style="display: flex; gap: 8px; align-items: center" class="mt-8">
+        <h2>Gebouwen</h2>
+        <div class="flex-grow-1"></div>
+        <RoundedButton icon="mdi-calendar" value="Wijzigen"></RoundedButton>
+      </div>
+
       <div class="grid">
         <img src="@/assets/images/dummyMap.png" class="map" alt="Map">
-        <div class="building-list space-y-8">
-          <CardLayout
-            class="building-card"
-            v-for="building in buildings"
-            v-bind:key="building.id"
-            @click="router.push({ name: 'building_id', params: { id: building.id } })"
-          >
-            <div>
-              <h3>{{building.name}}</h3>
-              <p>{{building.address.street}} {{building.address.number}}</p>
-              <p>{{building.address.zip_code}} {{building.address.city}} </p>
-            </div>
-            <div class="flex-grow-1"></div>
-            <v-icon icon="mdi-chevron-right"></v-icon>
-          </CardLayout>
-        </div>
+
+        <CardLayout
+          class="building-card"
+          v-for="building in buildings"
+          v-bind:key="building.id"
+          @click="router.push({ name: 'building_id', params: { id: building.id } })"
+        >
+          <div>
+            <h3>{{building.name}}</h3>
+            <p>{{building.address.street}} {{building.address.number}}</p>
+            <p>{{building.address.zip_code}} {{building.address.city}} </p>
+          </div>
+          <div class="flex-grow-1"></div>
+          <v-icon icon="mdi-chevron-right"></v-icon>
+        </CardLayout>
       </div>
+
+
 
       <div style="display: flex; gap: 8px; align-items: center" class="mt-8">
         <h2>Planning</h2>
         <div class="flex-grow-1"></div>
-        <RoundedButton icon="mdi-calendar" value="Volledige planning"></RoundedButton>
+        <RoundedButton icon="mdi-calendar" value="Inplannen"></RoundedButton>
       </div>
 
       <div class="space-y-8">
         <CardLayout
-          :class="{ 'bg-green-lighten-4': schedule === schedules[0] }"
           class="building-card"
           v-for="schedule in schedules"
           v-bind:key="schedule.id"
@@ -50,7 +50,7 @@
             <p>{{schedule.user.first_name}} {{schedule.user.last_name}}</p>
           </div>
           <div class="flex-grow-1"></div>
-          <RoundedButton v-if="schedule === schedules[0]" icon="mdi-bicycle-cargo" value="Actief"></RoundedButton>
+          <RoundedButton class="bg-green-lighten-5" v-if="schedule === schedules[0]" icon="mdi-bicycle-cargo" value="Actief"></RoundedButton>
           <RoundedButton v-if="schedule === schedules[0]" icon="mdi-image" value="13"></RoundedButton>
           <RoundedButton v-if="schedule === schedules[0]" icon="mdi-note-edit-outline" value="10"></RoundedButton>
           <v-icon v-if="schedule === schedules[0]" icon="mdi-chevron-right"></v-icon>
@@ -70,7 +70,7 @@
       <div style="display: flex; gap: 8px; align-items: center" class="mt-8">
         <h2>Afgelopen bezoeken</h2>
         <div class="flex-grow-1"></div>
-        <RoundedButton icon="mdi-calendar" value="Alle bezoeken"></RoundedButton>
+        <RoundedButton icon="mdi-history" value="Volledige geschiedenis"></RoundedButton>
       </div>
 
       <div class="space-y-8">
@@ -117,14 +117,15 @@ tryOrAlertAsync(async () => {
 
 <style scoped lang="sass">
 .map
+  grid-column: span 2 / span 2
   object-fit: cover
   width: 100%
-  aspect-ratio: 1
+  aspect-ratio: 3
   border-radius: 5px
 
 .grid
   display: grid
-  gap: 16px
+  gap: 8px
   grid-template-columns: repeat(2, minmax(0, 1fr))
 
 .space-y-8
@@ -137,10 +138,10 @@ tryOrAlertAsync(async () => {
   overflow: scroll
 
 .building-card
-  padding: 16px 0 16px 16px
+  padding: 24px 0 24px 24px
   display: flex
   align-items: center
-  gap: 16px
+  gap: 24px
 
 .schedule-card
   & > *
@@ -156,7 +157,7 @@ tryOrAlertAsync(async () => {
 
   .carousel
     display: flex
-    gap: 16px
+    gap: 24px
     overflow: scroll
 
   ul
--- web/src/views/round/RoundDetail.vue
+++ web/src/views/round/RoundDetail.vue
@@ -1,25 +1,25 @@
 <template>
   <div v-if="data !== null">
-    <HFillWrapper >
+    <HFillWrapper
+    >
       <div class="space-y">
-        <div style="display: flex; align-items: center; gap: 12px">
-          <h2>Ronde Korenmarkt</h2>
+        <div style="margin-left: 70px; display: flex; align-items: center; gap: 12px">
+          <h2>{{data.round.name}}</h2>
           <div class="flex-grow-1"></div>
-          <RoundedButton icon="mdi-calendar" :value="new Date().toLocaleDateString()"></RoundedButton>
+          <RoundedButton icon="mdi-calendar" :value="new Date(data.day).toLocaleDateString()"></RoundedButton>
           <RoundedButton icon="mdi-account" :value="data.user.first_name + ' ' + data. user.last_name"></RoundedButton>
         </div>
 
-        <p>
+        <p style="margin-left: 70px;">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
 
-        <divider-layout class="my-8"></divider-layout>
+        <divider-layout class="my-8" style="margin-left: 70px;"></divider-layout>
 
         <v-timeline
           truncate-line="both"
           side="end"
           density="compact"
-          style="margin-left: -70px"
         >
           <v-timeline-item
             :dot-color="'success'"
@@ -30,40 +30,50 @@
 
 
           <v-timeline-item
-            v-for="building in data.round.buildings.map(e => e.building)" v-bind:data="building.id"
+            v-for="entry in data.round.buildings.map(e => { return { building: e.building, progress: progressItems.get(e.building_id)}})" v-bind:key="entry.building.id"
             :dot-color="'success'"
             :icon="'mdi-check'"
             :size="'large'"
-            style="width: 100%"
           >
-
-            <CardLayout class="pa-4 pb-0 space-y">
-              <div style="align-items: center; display: flex; ">
+            <CardLayout class="pa-8 pb-0 space-y">
+              <div style="align-items: center; display: flex; cursor: pointer; gap: 12px" @click="router.push({ name: 'building_id', params: { id: entry.building.id } })">
                 <div>
-                  <h3>{{building.name}}</h3>
-                  <p>{{building.address.street}} {{building.address.number}}</p>
-                  <p>{{building.address.zip_code}} {{building.address.city}} </p>
+                  <h2>{{entry.building.name}}</h2>
+                  <p>{{entry.building.address.street}} {{entry.building.address.number}}</p>
+                  <p>{{entry.building.address.zip_code}} {{entry.building.address.city}} </p>
                 </div>
                 <div class="flex-grow-1"></div>
+
+                <RoundedButton v-if="entry.progress" icon="mdi-clock-start" :value="new Date(entry.progress.arrival).toLocaleTimeString()"></RoundedButton>
+                <RoundedButton v-if="entry.progress" icon="mdi-clock-end" :value="new Date(entry.progress.departure).toLocaleTimeString()"></RoundedButton>
+
                 <v-icon icon="mdi-chevron-right"></v-icon>
               </div>
 
-              <divider-layout></divider-layout>
+              <divider-layout v-if="entry.progress?.report"></divider-layout>
 
-              <div>
-                <h4 class="mb-2">Notities</h4>
-                <ul style="list-style: inside" v-for="i in [0, 1, 2]" v-bind:key="i">
-                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                </ul>
+              <div v-if="entry.progress?.report">
+                <div style="display: flex; align-items: center">
+                  <h3 class="mb-2">Notities</h3>
+                  <div class="flex-grow-1"></div>
+                </div>
+                <p>{{entry.progress.report}}</p>
+                <RoundedButton icon="mdi-pencil" value="Bewerken" class="mt-4"></RoundedButton>
               </div>
 
               <divider-layout></divider-layout>
 
               <div>
-                <h4 class="mb-2">Afbeeldingen</h4>
-                <div class="carousel">
-                  <img src="https://unsplash.com/photos/u_khkgVDmxA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8YmFzZW1lbnR8ZW58MHx8fHwxNjgxNzA5NzIx&force=true&w=640">
+                <h3 class="mb-2">Afbeeldingen</h3>
+                <div class="carousel" v-if="entry.progress?.images.length ?? 0 > 0">
+                  <div class="carousel-item" v-for="image in entry.progress.images">
+                    <img src="https://unsplash.com/photos/u_khkgVDmxA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8YmFzZW1lbnR8ZW58MHx8fHwxNjgxNzA5NzIx&force=true&w=640" >
+                    <p style="font-weight: 700; font-size: 14px;" class="mt-2">{{image.type}}</p>
+                    <p style="opacity: 90%">{{image.description}}</p>
+                  </div>
                 </div>
+                <p v-else style="opacity: 75%">Geen foto's toegevoegd.</p>
+                <RoundedButton icon="mdi-plus" value="Toevoegen" class="mt-4"></RoundedButton>
               </div>
             </CardLayout>
           </v-timeline-item>
@@ -73,10 +83,10 @@
             :dot-color="'grey'"
             :icon="'mdi-clock'"
           >
-            <h3>Einde</h3>
+            <h3>Einde: 14u00</h3>
           </v-timeline-item>
         </v-timeline>
-      </div>
+        </div>
     </HFillWrapper>
   </div>
 </template>
@@ -86,26 +96,39 @@ import {Ref, ref} from "vue";
 import DividerLayout from "@/layouts/DividerLayout.vue";
 import HFillWrapper from "@/layouts/HFillWrapper.vue";
 import CardLayout from "@/layouts/CardLayout.vue";
-import {Result, ScheduleQuery} from "@selab-2/groep-1-query";
+import {ProgressQuery, Result, ScheduleQuery} from "@selab-2/groep-1-query";
 import {tryOrAlertAsync} from "@/try";
 import RoundedButton from "@/components/buttons/RoundedButton.vue";
+import router from "@/router";
 
 const data: Ref<Result<ScheduleQuery> | null> = ref(null);
+const progressItems: Ref<Map<Number, Result<ProgressQuery>>> = ref(new Map());
 
 tryOrAlertAsync(async () => {
-  data.value = (await new ScheduleQuery().getAll({take: 1})).at(0) ?? null;
-})
+  data.value = await new ScheduleQuery().getOne(35);
+});
+
+tryOrAlertAsync(async () => {
+  for (const progress of await new ProgressQuery().getAll()) {
+    progressItems.value.set(progress.building_id, progress);
+  }
+});
 </script>
 
 <style lang="sass">
 .space-y
   & > *
-    margin-bottom: 12px
+    margin-bottom: 24px
 
 .carousel
   img
-    height: 200px
+    width: 100%
     aspect-ratio: 1
     border-radius: 5px
     overflow: clip
+    object-fit: cover
+
+  display: grid
+  grid-template-columns: repeat(3, minmax(0, 1fr))
+  gap: 24px
 </style>
