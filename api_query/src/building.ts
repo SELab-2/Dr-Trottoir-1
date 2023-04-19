1d19097b87c3dfa2408dc0b254b6fab59af0abc5
Janne Cools <janne.cools@ugent.be>
2023-04-19 09:35:32 +0200

    feature: new requests for BuildingImage in building.ts

--- api_query/src/building.ts
+++ api_query/src/building.ts
@@ -1,6 +1,7 @@
-import { Building, Prisma } from "@selab-2/groep-1-orm";
+import { Building, Image, Prisma } from "@selab-2/groep-1-orm";
 import { Query } from "./query";
 import { includeUserWithoutAddress } from "./include";
+import { QueryError } from "./query_error";
 
 export type BuildingQueryParameters = {
     take: number;
@@ -41,4 +42,40 @@ export class BuildingQuery extends Query<
     BuildingAllInfo
 > {
     endpoint = "building";
+
+    /**
+     * Voeg een nieuwe afbeelding toe via HTTP POST.
+     * @throws QueryError
+     */
+    async createImage(
+        id: number,
+        element: Partial<Image>,
+    ): Promise<BuildingAllInfo> {
+        if (Number.isNaN(id)) {
+            throw new QueryError(400, "Bad Request");
+        }
+
+        const imageEndpoint = this.server + this.endpoint + "/" + id + "/image";
+
+        return this.fetchJSON(imageEndpoint, "POST", element);
+    }
+
+    /**
+     * Verwijder een specifieke afbeelding via HTTP DELETE
+     * @throws QueryError
+     */
+    async deleteImage(
+        id: number,
+        image_id: number,
+        hard = false,
+    ): Promise<void> {
+        if (Number.isNaN(id) || Number.isNaN(image_id)) {
+            throw new QueryError(400, "Bad Request");
+        }
+
+        const imageEndpoint =
+            this.server + this.endpoint + "/" + id + "/image/" + image_id;
+
+        return this.fetchJSON(imageEndpoint, "DELETE", { hardDelete: hard });
+    }
 }
