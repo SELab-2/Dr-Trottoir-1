8eb6702304ce83fa0762a9588c760b79b9209a7c
Jens Pots <jens@jenspots.com>
2023-04-23 15:12:32 +0200

    fix: updated docker for use with token

--- server/generate_data.sh
+++ server/generate_data.sh
@@ -4,21 +4,38 @@
 # exit 2 => Could not enter mock directory
 
 ### update the DB schema ###
+echo "Entering ORM directory"
 cd ../orm || (echo >&2 "Could not enter the orm directory!" && exit 1)
 # backup the .env if it exists
 if [[ -f ".env" ]]; then
+    echo "Backing up .env file"
     cp .env .env.bak
 fi
 
+echo "Installing new .env file"
 cp ../server/developer.env .env
+
+echo "$ npm install"
 npm install
+
+echo "$ prisma db push"
 npx prisma db push
 
 # load the mock data
+echo "Entering API directory"
 cd ../api || (echo >&2 "Could not enter the API directory!" && exit 2)
+
 # backup the .env if it exists
 if [[ -f ".env" ]]; then
+    echo "Backing up .env file"
     cp .env .env.bak
 fi
+
+echo "Installing new .env file"
+cp ../server/developer.env .env
+
+echo "$ npm install"
 npm install
+
+echo "$ npm run mock"
 npm run mock
