7506f93e402c2ab9eb3f93866f8b056c98a9936c
Ludovic <ludovic.verstraete@ugent.be>
2023-04-29 15:22:37 +0200

    feat: mock data template

--- api/__tests__/mock/database.ts
+++ api/__tests__/mock/database.ts
@@ -14,6 +14,7 @@ import {
 import { initialiseRound, initialiseRoundBuilding } from "./round";
 import { initialiseFile, initialiseImage } from "./file";
 import { initialiseAction, initialiseGarbage } from "./garbage";
+import { initialiseMailTemplate} from "./mail_template"
 import { prisma } from "./prisma";
 import { Prisma } from "@selab-2/groep-1-orm";
 
@@ -38,6 +39,7 @@ const initialiseFunctions: { [name: string]: () => Promise<any> } = {
     schedule: initialiseSchedule,
     progress: initialiseProgress,
     progress_image: initialiseProgressImage,
+    mail_template: initialiseMailTemplate,
 };
 
 export async function initialiseDatabase(): Promise<void> {
