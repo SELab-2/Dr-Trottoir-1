3c1a4617e35b41b60756c35c729b8cb66f0488a7
Jens Pots <jens@jenspots.com>
2023-05-03 15:26:41 +0200

    fix: removed `tempalates`

--- api/__tests__/mock/mail_template.ts
+++ api/__tests__/mock/mail_template.ts
@@ -1,24 +1,24 @@
 import { prisma } from "./prisma";
 
 export async function initialiseMailTemplate() {
-    const mailTempalate1 = {
+    const mailTemplate1 = {
         name: "Vuilnis",
         mail_subject: "Vuilnis in $(building)",
         content: "In $(building) ligt er vuilnis op de grond",
     };
 
-    const mailTempalate2 = {
+    const mailTemplate2 = {
         name: "Code",
         mail_subject: "Code werkt niet in $(building)",
         content: "In $(building) werkt $(code) niet meer",
     };
 
-    const mailTempalate3 = {
+    const mailTemplate3 = {
         name: "Ivago",
         mail_subject: "Ivago is niet langs $(building) gekomen",
         content: "Ivago heeft $(garbage_type) niet meegenomen bij $(building)",
     };
     await prisma.mailTemplate.createMany({
-        data: [mailTempalate1, mailTempalate2, mailTempalate3],
+        data: [mailTemplate1, mailTemplate2, mailTemplate3],
     });
 }
