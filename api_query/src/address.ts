476ce4a55c51202698b88d2a525aa1bdb1240428
Jens Pots <jens@jenspots.com>
2023-05-03 16:08:51 +0200

    fix(api_query): updated address route

--- api_query/src/address.ts
+++ api_query/src/address.ts
@@ -2,5 +2,5 @@ import { Address } from "@selab-2/groep-1-orm";
 import { Query } from "./query";
 
 export class AddressQuery extends Query<void, Address, Address> {
-    endpoint = "action";
+    endpoint = "address";
 }
