a732e8e73bfa160380ac43a61ddd8f0de264f27f
Arnout_Allaert <arnout.allaert@ugent.be>
2023-05-08 17:30:21 +0200

    feat: pwa support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -23,6 +23,32 @@ export default ({ mode }) => {
       }),
       VitePWA({
         registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+          name: 'DrTrottoir',
+          short_name: 'DrTrottoir',
+          description: 'Website van DrTrottoir',
+          theme_color: '#ffffff',
+          start_url: '/',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
         devOptions: {
           enabled: true
         }
