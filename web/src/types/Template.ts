a8e57fb8195c8e12c0c8a6f4e0603b837eb72a3c
arne <vanheule.arne@ugent.be>
2023-05-16 16:39:06 +0200

    feat: possible to delete templates now

--- web/src/types/Template.ts
+++ web/src/types/Template.ts
@@ -2,6 +2,7 @@ import { Header } from "@/components/table/Header";
 import { TableEntity } from "@/components/table/TableEntity";
 import { RowType } from "@/components/table/RowType";
 import { MailTemplateQuery, Result } from "@selab-2/groep-1-query";
+import { tryOrAlertAsync } from "@/try";
 
 export class Template implements TableEntity<Result<MailTemplateQuery>> {
   headers(): Array<Header<Result<MailTemplateQuery>>> {
@@ -17,6 +18,24 @@ export class Template implements TableEntity<Result<MailTemplateQuery>> {
         get: (e: Result<MailTemplateQuery>) => e.name,
         type: RowType.TEXT,
         sortable: true,
+      },
+      {
+        id: 4,
+        name: "",
+        fit: true,
+        get: () => "mdi-close",
+        type: RowType.ICONBUTTON,
+        sortable: false,
+        onClick: (
+          e: Result<MailTemplateQuery>,
+          list: Array<Result<MailTemplateQuery> | null>,
+        ) => {
+          tryOrAlertAsync(async () => {
+            await new MailTemplateQuery().deleteOne({ id: e.id });
+            const index = list.findIndex((x) => x === e);
+            list[index] = null;
+          });
+        },
       },
     ].map((e) => new Header<Result<MailTemplateQuery>>(e));
   }
