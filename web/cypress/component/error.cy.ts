b817cd33344b7d9bd40cff183c531531d57d8354
Arnout_Allaert <arnout.allaert@ugent.be>
2023-05-19 19:21:12 +0200

    test: errorhandler

---
+++ web/cypress/component/error.cy.ts
@@ -0,0 +1,7 @@
+import ErrorHandler from '@/components/errors/ErrorHandler.vue'
+
+describe("error handler test", () => {
+  it('error handler test', () => {
+    // this cant really be tested, since it reacts to errors not props
+  })
+})
