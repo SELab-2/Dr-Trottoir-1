d4aae37e6969637991133470dbcb2bac8b241b63
Jozef Jankaj <jozef.jankaj@ugent.be>
2023-05-19 17:15:50 +0200

    Add extra options for the scripts

--- orm/scripts/backup.sh
+++ orm/scripts/backup.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
-
+# variables
 CURRENT_DATE=$(date +"%d-%m-%Y_%H-%M-%S")
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_NAME="main"
 
-echo "Backing up database schema"
+function help() {
+        echo "Usage: backup.sh (-h hostname) (-p port) (-d database_name)"
+        exit 1
+}
 
+while getopts ":h:p:d:" opt; do
+        case "$opt" in
+                h) DB_HOST="$OPTARG" ;;
+                p) DB_PORT="$OPTARG" ;;
+                d) DB_NAME="$OPTARG" ;;
+                \?) help ;;
+        esac
+done
+
+shift "$((OPTIND-1))"
+
+echo "Backing up database $DB_NAME"
 # generate schema backup
-read -p "DB username: " username
-pg_dump --clean --create --column-inserts -h localhost -p 5432 -U "$username" -d main > "$CURRENT_DATE.sql"
-echo "Database backup stored in $CURRENT_DATE.sql"
+read -rp "DB username: " username
+if pg_dump --clean --create --column-inserts -h "$DB_HOST" -p "$DB_PORT" -U "$username" -d "$DB_NAME" > "$CURRENT_DATE.sql"; then
+        echo "Database backup stored in $CURRENT_DATE.sql"
+fi
--- orm/scripts/restore.sh
+++ orm/scripts/restore.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_NAME="main"
+
+function help() {
+        echo "Usage: restore.sh (-h hostname) (-p port) (-d db_name) backup_file"
+        exit 1
+}
+
+while getopts ":h:p:d:" opt; do
+        case "$opt" in
+                h) DB_HOST="$OPTARG" ;;
+                p) DB_PORT="$OPTARG" ;;
+                d) DB_NAME="$OPTARG" ;;
+                \?) help ;;
+        esac
+done
+
+shift "$((OPTIND-1))"
+
+if [ -z "$1" ]; then
+        echo "Backup file must be provided"
+        exit 1
+fi
+
+
 echo "Restoring DB from backup"
 echo "WARNING: this script will REMOVE all present data!"
 
-read -p "DB username: " username
+read -rp "DB username: " USERNAME
 
 echo "Dropping current database"
-dropdb -h localhost -p 5432 -U "$username" main --force
+if dropdb -h "$DB_HOST" -p "$DB_PORT" -U "$USERNAME" "$DB_NAME" --force; then
+        echo "Restoring database from backup file $1"
+else
+        echo "Unable to drop current database"
+fi
 
-echo "Restoring database from backup file $1"
-psql -h localhost -p 5432 -U "$username" -f "$1"
+if psql -h "$DB_HOST" -p "$DB_PORT" -U "$USERNAME" -f "$1" > /dev/null 2>&1; then
+        echo "Database restored"
+else
+        echo "Was unable to restore from file $1"
+fi
